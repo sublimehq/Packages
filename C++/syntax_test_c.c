939e39f2a1c9b249640d97438d6878f6ba6359c9
TheClams <clams@mail.com>
2015-07-04 09:51:00 +0200

    [C] Fix issue around preprocessor
    Properly handle multiline #define/#error
    Highlight macro parameter as variable
    Add a test file showing issues with the C syntax that are now fixed with
    this commit.
    
    Also start moving some scope defined directly at the top in their
    context to ease their reuse.
    
    This fix a few issues reported:
    - SublimeTextIssues/DefaultPackages#36
    - SublimeTextIssues/DefaultPackages#68
    - SublimeTextIssues/DefaultPackages#112
    - SublimeTextIssues/DefaultPackages#126

---
+++ C++/syntax_test_c.c
@@ -0,0 +1,38 @@
+/* SYNTAX TEST "C.sublime-syntax" */
+
+#define EXTTS_BUFSIZE (PTP_BUF_TIMESTAMPS /* comment block */ * sizeof(struct ptp_extts_event)) // comment line
+/* <- keyword.other.preprocessor */
+/*      ^ entity.name.constant.preprocessor */
+/*                                        ^ comment.block */
+/*                                                              ^ keyword.operator.sizeof */
+/*                                                                     ^ storage.type */
+/*                                                                                              ^ comment.line */
+
+
+#define max(a, b, \
+/* <- keyword.other.preprocessor */ \
+/*      ^ entity.name.function.preprocessor */ \
+/*         ^ meta.preprocessor.params */ \
+/*          ^ variable.parameter */ \
+ c)  ((a>b) ? (a>c?a:c) : (b>c?b:c))
+ /* <- variable.parameter */
+
+#define PACKED __attribute__((aligned(1),packed))
+/*      ^ entity.name.constant */
+/*             ^ storage.modifier */
+
+int i;
+/* <- storage.type */
+
+
+#if defined(VARIABLE)
+# error This is a long error message that need to   \
+/* <- keyword.control.import */ \
+/*      ^ string.unquoted */ \
+    be splitted into two lines to prevent large lines. // comment
+#endif
+ /* <- keyword.control.import.if */
+
+bool still_C_code_here = true;
+/* <- storage.type */
+/*                       ^ constant.language */
