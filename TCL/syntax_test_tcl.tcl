2c7e0fa3224e74e34aace5627016905f123293c6
TheClams <clams@mail.com>
2015-07-22 08:50:34 +0200

    [TCL] Fix highlighting of escape character \\
    Fix issue  SublimeTextIssues/DefaultPackages#134
    
    Also add test file

---
+++ TCL/syntax_test_tcl.tcl
@@ -0,0 +1,27 @@
+# SYNTAX TEST "tcl.sublime-syntax"
+
+# -------------------------------------------------------------------------- #
+# Issue 134: https://github.com/SublimeTextIssues/DefaultPackages/issues/134
+# -------------------------------------------------------------------------- #
+regsub -all {\/} $line {\\} line;
+# <- keyword.other
+#            ^ string.regexp
+#                       ^ constant.character.escape
+regsub -all {\\\\} $line {\\} line;
+# <- keyword.other
+#            ^ string.regexp constant.character.escape
+#                         ^ constant.character.escape
+puts "Not Highlighting"
+# <- keyword.other
+#     ^ string.quoted.double
+
+
+# -------------------------------------------------------------------------- #
+# Issue 131: https://github.com/SublimeTextIssues/DefaultPackages/issues/134
+# -------------------------------------------------------------------------- #
+set ok1 {["]"]}
+set ok2 {["][]"]}
+set not_ok {["]["]}
+puts $ok1            ;# ["]"]
+puts $ok2            ;# ["][]"]
+puts $not_ok         ;# ["]["]
