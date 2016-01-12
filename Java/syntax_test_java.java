e682b30de6dffedcb41d12237fd54a288cf6fff0
wbond <will@wbond.net>
2016-01-12 16:04:28 -0500

    [Java] Add a test for method reference usage of "new"

---
+++ Java/syntax_test_java.java
@@ -0,0 +1,20 @@
+// SYNTAX TEST "Java.sublime-syntax"
+
+public class SyntaxTest {
+    public static void main(String... args) {
+        String[] strings = new String[5];
+//                         ^ keyword.control.new.java
+        printList(Arrays.stream(args)
+            .collect(Collectors.toCollection(ArrayList::new)));
+//                                                      ^ meta.method.body.java - keyword.control.new.java
+        anotherMethod();
+    }
+
+    private static void printList(List<String> args) {
+        args.stream().forEach(System.out::println);
+    }
+
+    private static void anotherMethod() {
+
+    }
+}
