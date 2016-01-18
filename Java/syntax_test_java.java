2d90735e48622c47de7bc58f49fa981aa064dd3e
Rachel Greenham <rachel@strangenoises.org>
2016-01-18 16:36:13 +0000

    add a test for string literal

--- Java/syntax_test_java.java
+++ Java/syntax_test_java.java
@@ -5,9 +5,9 @@ public class SyntaxTest {
     private String memberString2 = new String("Hello");
     private String memberString3 = String.valueOf("Hello");
 //                                ^ meta.assignment.rhs.java
+//                                                 ^ string.quoted.double.java
     private int memberLpos = memberString3.indexOf("l");
 //                                                     ^ punctuation.terminator.java
-    private String memberClassPath = System.getProperty("java.class.path");
 
     public static void main(String... args) {
         String[] strings = new String[5];
