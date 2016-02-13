50add635c2c184d513a459ffdc8b70306043bb60
John Wilkes <jwilkes0011@gmail.com>
2016-02-13 15:09:43 -0600

    Fixing a bug where @rem was not highlighted as a comment

---
+++ Batch File/syntax_test_batch.bat
@@ -0,0 +1,9 @@
+rem SYNTAX TEST "Packages/Batch File/Batch File.sublime-syntax"
+
+rem comment
+rem <- keyword.command.rem
+rem ^ comment.line.rem
+
+@rem comment
+ rem <- keyword.command.rem
+rem  ^ comment.line.rem
