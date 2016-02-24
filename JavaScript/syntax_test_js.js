sublimehq/Packages

commit 6ac14237a21e24f8c2f39bbf2361c12c98e2d85c

[JavaScript] Fix bug with variables sometimes being highlighted as a property

JavaScript/JavaScript.sublime-syntax
@@ -382,9 +382,9 @@ contexts:
             1: punctuation.definition.string.end.js
             2: keyword.other.js
           pop: true
-        - match: (?=.)
+        - match: '(?=[.\n])'
           with_prototype:
-            - match: (?=/)
+            - match: '(?=/)'
               pop: true
           push: scope:source.regexp.js
 
@@ -1022,7 +1022,7 @@ contexts:
       scope: meta.property.object.js
     - match: \b(s(hape|ystemId|c(heme|ope|rolling)|ta(ndby|rt)|ize|ummary|pecified|e(ctionRowIndex|lected(Index)?)|rc)|h(space|t(tpEquiv|mlFor)|e(ight|aders)|ref(lang)?)|n(o(Resize|tation(s|Name)|Shade|Href|de(Name|Type|Value)|Wrap)|extSibling|ame)|c(h(ildNodes|Off|ecked|arset)?|ite|o(ntent|o(kie|rds)|de(Base|Type)?|l(s|Span|or)|mpact)|ell(s|Spacing|Padding)|l(ear|assName)|aption)|t(ype|Bodies|itle|Head|ext|a(rget|gName)|Foot)|i(sMap|ndex|d|m(plementation|ages))|o(ptions|wnerDocument|bject)|d(i(sabled|r)|o(c(type|umentElement)|main)|e(clare|f(er|ault(Selected|Checked|Value)))|at(eTime|a))|useMap|p(ublicId|arentNode|r(o(file|mpt)|eviousSibling))|e(n(ctype|tities)|vent|lements)|v(space|ersion|alue(Type)?|Link|Align)|URL|f(irstChild|orm(s)?|ace|rame(Border)?)|width|l(ink(s)?|o(ngDesc|wSrc)|a(stChild|ng|bel))|a(nchors|c(ce(ssKey|pt(Charset)?)|tion)|ttributes|pplets|l(t|ign)|r(chive|eas)|xis|Link|bbr)|r(ow(s|Span|Index)|ules|e(v|ferrer|l|adOnly))|m(ultiple|e(thod|dia)|a(rgin(Height|Width)|xLength))|b(o(dy|rder)|ackground|gColor))\b
       scope: support.constant.dom.js
-    - match: '(?=.)'
+    - match: '(?=[.\n])'
       pop: true
 
   literal-punctuation:
@@ -1049,10 +1049,10 @@ contexts:
         - match: '(?={{identifier}}\s*\()'
           set:
             - include: method-call
-            - match: (?=.)
+            - match: '(?=[.\n])'
               pop: true
         - include: object-property
-        - match: '(?=.)'
+        - match: '(?=[.\n])'
           pop: true
     - match: \;
       scope: punctuation.terminator.statement.js

JavaScript/syntax_test_js.js
@@ -197,7 +197,6 @@ $foo = null;
 // <- variable.other.dollar punctuation.dollar
 // ^ variable.other.dollar - punctuation.dollar
 
-
 baz = "";
 // <- variable.other.readwrite
 //     ^ string.quoted.double
@@ -440,6 +439,10 @@ new Date().getTime()
 //                             ^ meta.group.braces.square
     'test3': "asdf"
 }
+
+a = b.c
+d = e.f
+// <- variable.other.readwrite
 
 width/2 + lineStart * Math.sin(i * 30 * π/180)
 //   ^ keyword.operator.arithmetic

commit 0de86593947458808d394a1d5a53ceaac95515d7

[JavaScript] Fixed a regression in 6ac14237a21e24f8c2f39bbf2361c12c98e2d85c

JavaScript/JavaScript.sublime-syntax
@@ -382,7 +382,7 @@ contexts:
             1: punctuation.definition.string.end.js
             2: keyword.other.js
           pop: true
-        - match: '(?=[.\n])'
+        - match: '(?=.|\n)'
           with_prototype:
             - match: '(?=/)'
               pop: true
@@ -1022,7 +1022,7 @@ contexts:
       scope: meta.property.object.js
     - match: \b(s(hape|ystemId|c(heme|ope|rolling)|ta(ndby|rt)|ize|ummary|pecified|e(ctionRowIndex|lected(Index)?)|rc)|h(space|t(tpEquiv|mlFor)|e(ight|aders)|ref(lang)?)|n(o(Resize|tation(s|Name)|Shade|Href|de(Name|Type|Value)|Wrap)|extSibling|ame)|c(h(ildNodes|Off|ecked|arset)?|ite|o(ntent|o(kie|rds)|de(Base|Type)?|l(s|Span|or)|mpact)|ell(s|Spacing|Padding)|l(ear|assName)|aption)|t(ype|Bodies|itle|Head|ext|a(rget|gName)|Foot)|i(sMap|ndex|d|m(plementation|ages))|o(ptions|wnerDocument|bject)|d(i(sabled|r)|o(c(type|umentElement)|main)|e(clare|f(er|ault(Selected|Checked|Value)))|at(eTime|a))|useMap|p(ublicId|arentNode|r(o(file|mpt)|eviousSibling))|e(n(ctype|tities)|vent|lements)|v(space|ersion|alue(Type)?|Link|Align)|URL|f(irstChild|orm(s)?|ace|rame(Border)?)|width|l(ink(s)?|o(ngDesc|wSrc)|a(stChild|ng|bel))|a(nchors|c(ce(ssKey|pt(Charset)?)|tion)|ttributes|pplets|l(t|ign)|r(chive|eas)|xis|Link|bbr)|r(ow(s|Span|Index)|ules|e(v|ferrer|l|adOnly))|m(ultiple|e(thod|dia)|a(rgin(Height|Width)|xLength))|b(o(dy|rder)|ackground|gColor))\b
       scope: support.constant.dom.js
-    - match: '(?=[.\n])'
+    - match: '(?=.|\n)'
       pop: true
 
   literal-punctuation:
@@ -1049,11 +1049,9 @@ contexts:
         - match: '(?={{identifier}}\s*\()'
           set:
             - include: method-call
-            - match: '(?=[.\n])'
+            - match: '(?=.|\n)'
               pop: true
         - include: object-property
-        - match: '(?=[.\n])'
-          pop: true
     - match: \;
       scope: punctuation.terminator.statement.js
     - match: ","

JavaScript/syntax_test_js.js
@@ -455,9 +455,13 @@ var reg = /a+/gimy.exec('aabb')
 
 'aabbcc'.replace(/b+/, 'd')
 //               ^^^^ string.regexp
+//                 ^ keyword.operator.quantifier.regexp
 
 /a+/
 // <- string.regexp
+
+'foo'.bar() / baz
+//            ^ variable.other.readwrite
 
 var g = 50
 