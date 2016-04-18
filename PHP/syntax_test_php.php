fb90b4e81b2559a7a2db92d00adac28c8f1c9b73
wbond <will@wbond.net>
2016-04-18 15:29:00 -0400

    [PHP] Condense syntax test files

--- PHP/syntax_test_php.php
+++ PHP/syntax_test_php.php
@@ -95,3 +95,37 @@ $test = "\0 \12 \345g \x0f \u{a} \u{9999} \u{999}";
 //                         ^^^^^ constant.numeric.unicodepoint.php
 //                               ^^^^^^^^ constant.numeric.unicodepoint.php
 //                                        ^^^^^^^ meta.string-contents.quoted.double.php
+
+trait A
+// ^ storage.type.trait.php
+{
+
+}
+
+class B
+{
+    use X,
+    Y,
+//  ^ support.other.namespace.use.php
+    Z {
+        X::method1 as another1;
+        //         ^ support.trait.method.use-as.php
+        //            ^ meta.function.php
+        Y::method2 insteadof X;
+        //         ^ support.trait.method.insteadof.php
+        //                   ^ support.class.php
+        X::method2 as another2;
+        //         ^ support.trait.method.use-as.php
+        //            ^ meta.function.php
+    } protected $pro1;
+    // ^ storage.modifier.php
+
+    public function abc(callable $var,          int $var2,          string $var3)
+    //                  ^ storage.type.php
+    //                                          ^ storage.type.php
+    //                                                              ^ storage.type.php
+    {
+        echo B::class;
+        //      ^ constant.class.php
+    }
+}
