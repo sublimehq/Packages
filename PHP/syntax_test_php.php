e69de845f2474abfcc5d1ebbdefe3969c561d898
wbond <will@wbond.net>
2016-04-18 14:57:08 -0400

    [PHP] Fix constant.numeric.unicodepoint to reflect form specified at http://php.net/manual/en/language.types.string.php

--- PHP/syntax_test_php.php
+++ PHP/syntax_test_php.php
@@ -86,12 +86,12 @@ $anon = new class extends Test1 implements Countable {};
 //                                     ^ support.class.php
 //                                                 ^ support.class.php
 
-$test = "\0 \12 \345g \x0f \u{aa} \u{9999} \u{999}";
+$test = "\0 \12 \345g \x0f \u{a} \u{9999} \u{999}";
 //       ^^ constant.numeric.octal.php
 //          ^^^ constant.numeric.octal.php
 //              ^^^^ constant.numeric.octal.php
 //                  ^ meta.string-contents.quoted.double.php
 //                    ^^^^ constant.numeric.hex.php
-//                         ^^^^^^ constant.numeric.unicodepoint.php
-//                                ^^^^^^^^ constant.numeric.unicodepoint.php
-//                                         ^^^^^^^ meta.string-contents.quoted.double.php
+//                         ^^^^^ constant.numeric.unicodepoint.php
+//                               ^^^^^^^^ constant.numeric.unicodepoint.php
+//                                        ^^^^^^^ meta.string-contents.quoted.double.php
