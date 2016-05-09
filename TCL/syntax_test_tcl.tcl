cfb6744894cb0465c7905ee00c76a188b3bdef9a
wbond <will@wbond.net>
2016-05-09 18:21:12 -0400

    [TCL] Rewrite of syntax

--- TCL/syntax_test_tcl.tcl
+++ TCL/syntax_test_tcl.tcl
@@ -3,6 +3,7 @@
 # -------------------------------------------------------------------------- #
 # Issue 134: https://github.com/SublimeTextIssues/DefaultPackages/issues/134
 # -------------------------------------------------------------------------- #
+
 regsub -all {\/} $line {\\} line;
 # <- keyword.other
 #            ^ string.regexp
@@ -15,16 +16,59 @@ puts "Not Highlighting"
 # <- keyword.other
 #     ^ string.quoted.double
 
+namespace eval A {
+#^^^^^^^^^^^^^^^^^ meta.namespace
+#              ^ entity.name.namespace
+    namespace eval B {
+#   ^^^^^^^^^^^^^^^^^^ meta.namespace meta.namespace
+#                  ^ entity.name.namespace
+        proc C  {
+#       ^^^^^^^^^^^ meta.function
+#            ^ entity.name.function
+            puts hi
+        }
+    }
+}
+
+set v "String with $var and [command substitution] in it. Also, \n escapes."
+#                  ^^^^ variable.other
+#                           ^^^^^^^^^^^^^^^^^^^^^^ meta.substitution
+#                            ^^^^^^^ variable.function
+#                                                               ^^ constant.character.escape
+
+expr 5 + 4
+#    ^ constant.numeric
+#        ^ constant.numeric
+
+proc D {a {b "default"} args} {
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.function
+#    ^ entity.name.function
+#      ^^^^^^^^^^^^^^^^^^^^^^ meta.function.parameters meta.block
+#       ^ variable.parameter
+#         ^^^^^^^^^^^^^ meta.block meta.block
+#          ^ variable.parameter
+#            ^^^^^^^^^ string.quoted.double
+#                       ^^^^ variable.parameter
+
+}
+# <- meta.function
+ # <- - meta.function
 
 # -------------------------------------------------------------------------- #
 # Issue 131: https://github.com/SublimeTextIssues/DefaultPackages/issues/131
 # -------------------------------------------------------------------------- #
 set ok1 {["]"]}
-#       ^^^^^^^ string.quoted.curly
+#       ^^^^^^^ meta.block
+#        ^^^^^ meta.substitution
+#         ^^^ string.quoted.double
 set ok2 {["][]"]}
-#       ^^^^^^^^^ string.quoted.curly
+#       ^^^^^^^^^ meta.block
+#        ^^^^^^^ meta.substitution
+#         ^^^^^ string.quoted.double
 set not_ok {["]["]}
-#          ^^^^^^^^ string.quoted.curly
+#          ^^^^^^^^ meta.block
+#           ^^^^^^ meta.substitution
+#            ^^^^ string.quoted.double
 puts $ok1            ;# ["]"]
 # ^ keyword.other
 #    ^^^^ variable.other
@@ -40,3 +84,33 @@ puts $not_ok         ;# ["]["]
 #    ^^^^^^^ variable.other
 #    ^ punctuation.definition.variable
 #                     ^ comment.line
+
+my-command param1 $param2
+# <- variable.function
+
+array 1 2 3
+#^^^^ keyword.other
+
+[ list foo baz]
+# ^ keyword.other
+#^^^^^^^^^^^^^^ meta.substitution
+
+if {$var == true} {
+# <- keyword.control
+#  ^^^^^^^^^^^^^^ meta.block
+#        ^^ keyword.operator
+#                 ^ meta.block
+
+} elseif {$foo < $bar} {
+# ^^^^^^ keyword.control
+#        ^^^^^^^^^^^^^ meta.block
+#              ^ keyword.operator
+
+} else {
+# ^^^^ keyword.control
+
+}
+
+# Ensure else doesn't work outside of if command
+else {}
+# <- - keyword.control
