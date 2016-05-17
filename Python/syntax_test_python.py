sublimehq/Packages

commit 0550509bcd570fe1afd584a9eb996d7cfb133cd1

[Python] "Paths" can have spaces surrounding dots

Python/Python.sublime-syntax
@@ -19,7 +19,7 @@ variables:
   # We support unicode here because Python 3 is the future
   identifier_continue: '[[:alnum:]_]'
   identifier: '\b[[:alpha:]_]{{identifier_continue}}*\b'
-  path: '({{identifier}}\.)*{{identifier}}'
+  path: '({{identifier}} *\. *)*{{identifier}}'
   sql_indicator: \s*(?:SELECT|INSERT|UPDATE|DELETE|CREATE|REPLACE|ALTER)\b
 
 contexts:
@@ -178,7 +178,7 @@ contexts:
                     - meta_scope: entity.other.inherited-class.python
                     - include: identifier-parts
                     - match: '{{identifier}}'
-                    - match: ''
+                    - match: '(?=\S)'
                       pop: true
                 - include: expressions
 
@@ -245,14 +245,14 @@ contexts:
       scope: variable.parameter.python
 
   decorators:
-    - match: '^\s*(?=@\s*{{identifier}}(?:\.{{identifier}})*\s*\()'
+    - match: '^\s*(?=@\s*{{path}}\s*\()'
       comment: a decorator may be a function call which returns a decorator.
       push:
         - meta_scope: meta.function.decorator.python
         - match: \)
           scope: punctuation.definition.arguments.end.python
           pop: true
-        - match: '(?=(@)\s*{{identifier}}(?:\.{{identifier}})*\s*\()'
+        - match: '(?=(@)\s*{{path}}\s*\()'
           captures:
             1: punctuation.definition.decorator.python
           push:
@@ -268,7 +268,7 @@ contexts:
               pop: true
             - include: keyword_arguments
             - include: expressions
-    - match: '^\s*(?=@\s*{{identifier}}(?:\.{{identifier}})*)'
+    - match: '^\s*(?=@\s*{{path}})'
       push:
         - meta_scope: meta.function.decorator.python
         - meta_content_scope: entity.name.function.decorator.python
@@ -283,13 +283,13 @@ contexts:
             - include: dotted_name
 
   item-access:
-    - match: '(?={{identifier}}(?:\.{{identifier}})*\s*\[)'
+    - match: '(?={{path}}\s*\[)'
       push:
         - meta_scope: meta.item-access.python
         - match: \]
           scope: punctuation.definition.arguments.end.python
           pop: true
-        - match: '(?={{identifier}}(?:\.{{identifier}})*\s*\[)'
+        - match: '(?={{path}}\s*\[)'
           push:
             - match: '(?=\s*\[)'
               pop: true
@@ -324,13 +324,13 @@ contexts:
           pop: true
         - include: keyword_arguments
         - include: expressions
-    - match: '(?={{identifier}}(?:\.{{identifier}})*\s*\()'
+    - match: '(?={{path}}\s*\()'
       push:
         - meta_scope: meta.function-call.python
         - match: \)
           scope: punctuation.definition.arguments.end.python
           pop: true
-        - match: '(?={{identifier}}(?:\.{{identifier}})*\s*\()'
+        - match: '(?={{path}}\s*\()'
           push:
             - match: (?=\s*\()
               pop: true
@@ -409,7 +409,7 @@ contexts:
         - include: expressions
 
   identifier-parts:
-    - match: '{{identifier}}(\.)'
+    - match: '{{identifier}} *(\.)'
       captures:
         1: punctuation.accessor.python
 
@@ -451,31 +451,39 @@ contexts:
   constant_placeholder:
     - match: '(?i:(%(\([a-z_]+\))?#?0?\-?[ ]?\+?([0-9]*|\*)(\.([0-9]*|\*))?[hL]?[a-z%])|(\{([!\[\].:\w ]+)?\}))'
       scope: constant.other.placeholder.python
-  dotted_name:
-    - match: '(?={{identifier}}(?:\.{{identifier}})*)'
+
+  name:
+    - match: '(?={{identifier}})'
       push:
-        - match: '(?![{{identifier_continue}}\.])'
+        - meta_scope: meta.name.python
+        - match: '(?!{{identifier_continue}})'
           pop: true
-        - match: '(\.)(?={{identifier}})'
+        - include: builtin_functions
+        - include: builtin_types
+        - include: builtin_exceptions
+        - include: illegal_names
+        - include: magic_function_names
+        - include: magic_variable_names
+        - include: language_variables
+        - include: generic_names
+
+  dotted_name:
+    - match: '(?={{path}})'
+      push:
+        - meta_scope: meta.name.dotted.python
+        - match: '(?!{{identifier_continue}}| *\.)'
+          pop: true
+        - match: '(\.)(?= *{{identifier}})'
+          scope: punctuation.accessor.python
           push:
-            - match: "(?!{{identifier_continue}})"
+            - match: "(?! *{{identifier_continue}})"
               pop: true
             - include: magic_function_names
             - include: magic_variable_names
             - include: illegal_names
             - include: generic_names
-        - match: '(?={{identifier}})'
-          push:
-            - match: "(?!{{identifier_continue}})"
-              pop: true
-            - include: builtin_functions
-            - include: builtin_types
-            - include: builtin_exceptions
-            - include: illegal_names
-            - include: magic_function_names
-            - include: magic_variable_names
-            - include: language_variables
-            - include: generic_names
+        - include: name
+
   entity_name_class:
     - include: illegal_names
     - include: generic_names

Python/syntax_test_python.py
@@ -6,6 +6,14 @@ import path from os
 #^^^^^ keyword.control.import
 #           ^^^^ keyword.control.import.from
 
+name
+#^^^ meta.name
+#
+name1 . name2
+#^^^^^^^^^^^^ meta.name.dotted
+#     ^ punctuation.accessor
+#^^^^ meta.name.dotted meta.name
+
 def my_func(param1, # Multi-line function definition
 #                   ^ comment.line.number-sign
     # This is defaulted
@@ -96,9 +104,9 @@ class MyClass(Inherited,
 #     ^^^^^^^ entity.name.type.class
 #             ^^^^^^^^^ entity.other.inherited-class
 #                      ^ punctuation.separator
-              module.Inherited2):
-#             ^^^^^^^^^^^^^^^^^ entity.other.inherited-class
-#                   ^ punctuation.accessor
+              module . Inherited2):
+#             ^^^^^^^^^^^^^^^^^^^ entity.other.inherited-class
+#                    ^ punctuation.accessor
     """
     This is a test of docstrings
     """

commit d4114d254d393453b6542732a29bd7d223c24fee

[Python] Properly exit dotted_name context
Fixes a regression introduced by
0550509bcd570fe1afd584a9eb996d7cfb133cd1 where dotted names containing
at least one dot would not be terminated by "just" spaces.

Python/Python.sublime-syntax
@@ -647,17 +647,18 @@ contexts:
     - match: '(?={{path}})'
       push:
         - meta_scope: meta.name.dotted.python
-        - match: '(?!{{identifier_continue}}| *\.)'
-          pop: true
-        - match: '(\.)(?= *{{identifier}})'
-          scope: punctuation.accessor.python
+        - match: ' *(\.) *(?={{identifier}})'
+          captures:
+            1: punctuation.accessor.python
           push:
-            - match: "(?! *{{identifier_continue}})"
+            - match: '(?!{{identifier_continue}})'
               pop: true
             - include: magic_function_names
             - include: magic_variable_names
             - include: illegal_names
             - include: generic_names
+        - match: '(?!{{identifier_continue}})'
+          pop: true
         - include: name
 
   entity_name_class:

Python/syntax_test_python.py
@@ -11,6 +11,8 @@ from os import path, chdir # comment
 #                  ^ punctuation.separator.import-list
 #                    ^^^^^ meta.name
 #                          ^ comment
+from collections.abc import Iterable
+#                    ^^^^^^ keyword.control.import
 from \
     os \
     import \
@@ -40,13 +42,15 @@ from a import (b as c)
 import re; re.compile(r'')
 #        ^^^^^^^^^^^^^^^^^ - meta.statement.import
 #        ^ punctuation.separator.statements
+
 name
 #^^^ meta.name
-#
 name1 . name2
 #^^^^^^^^^^^^ meta.name.dotted
 #     ^ punctuation.accessor
 #^^^^ meta.name.dotted meta.name
+name .2name
+#     ^ - meta.name
 
 class
 #^^^^ storage.type.class