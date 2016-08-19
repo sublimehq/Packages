e64d43dbc5b588a8f9c009bd4f19c8c43175c414
urraka <perroazul64@gmail.com>
2016-08-19 12:16:02 -0300

    Added tests for comment scopes

--- Pascal/syntax_test.pas
+++ Pascal/syntax_test.pas
@@ -1,5 +1,15 @@
 // SYNTAX TEST "Packages/Pascal/Pascal.sublime-syntax"
 
+  // double slash comment
+// <- punctuation.whitespace.comment.leading.pascal
+  // <- punctuation.definition.comment.pascal
+  // ^^^^^^^^^^^^^^^^^^^^ comment.line.double-slash.pascal.two
+
+  -- double dash comment
+// <- punctuation.whitespace.comment.leading.pascal
+  // <- punctuation.definition.comment.pascal
+  // ^^^^^^^^^^^^^^^^^^^ comment.line.double-dash.pascal.one
+
 // comment
 procedure foo;
 // ^ meta.function.pascal
