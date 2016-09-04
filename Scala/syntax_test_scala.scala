09158c776f687495d445176adad98a04e2b658d2
Daniel Spiewak <djspiewak@gmail.com>
2016-09-04 12:56:19 -0600

    [Scala] Avoid catching xml-prefixed preproc instrs as invalid

--- Scala/syntax_test_scala.scala
+++ Scala/syntax_test_scala.scala
@@ -1078,6 +1078,9 @@ def <(a: Int) = 42
    <?xml
 // ^^^^^ invalid.illegal.reserved-proc-instr.xml
 
+   <?xmll?>
+// ^^^^^^^^ - invalid
+
    <?foo thing="false"?>
 //   ^^^ entity.name.tag.xml
 //             ^^^^^^^ string.quoted.double.xml
