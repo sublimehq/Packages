57788183d493a38e70eff5b827bf093c282e7e49
evandrocoan <evandrocoan@hotmail.com>
2016-09-17 00:03:30 -0300

    Merged the file syntax_test_punctuation.m into syntax_test_matlab.m

--- Matlab/syntax_test_matlab.m
+++ Matlab/syntax_test_matlab.m
@@ -34,3 +34,26 @@ classdef (Sealed = false) classname < baseclass
 end
 
 
+
+%---------------------------------------------
+% Syntax brackets/parens punctuation test
+
+x = [ 1.76 ]
+%  <- source.matlab punctuation.definition.brackets.begin.matlab
+% ^ source.matlab keyword.operator.symbols.matlab
+%   ^ source.matlab punctuation.definition.brackets.begin.matlab 
+%     ^ source.matlab meta.brackets.matlab constant.numeric.matlab  
+%          ^ source.matlab punctuation.definition.brackets.end.matlab
+
+
+xAprox = fMetodoDeNewton( xi )
+%  <- source.matlab meta.variable.other.valid.matlab
+%      ^ source.matlab keyword.operator.symbols.matlab
+%        ^ source.matlab meta.variable.other.valid.matlab
+%                       ^ source.matlab punctuation.definition.parens.begin.matlab 
+%                         ^ source.matlab meta.parens.matlab meta.variable.other.valid.matlab
+%                            ^ source.matlab punctuation.definition.parens.end.matlab 
+
+
+
+
