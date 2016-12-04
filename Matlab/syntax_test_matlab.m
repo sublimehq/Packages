f2ef2415fcc60d03aac1e249212026092973c961
evandrocoan <evandrocoan@hotmail.com>
2016-12-04 13:48:41 -0200

    Added the Unit Test for the new fixes. Success: 30 assertions in 1
    files passed
    [Finished]

--- Matlab/syntax_test_matlab.m
+++ Matlab/syntax_test_matlab.m
@@ -35,14 +35,15 @@ end
 
 
 
+
 %---------------------------------------------
 % Syntax brackets/parens punctuation test
 
 x = [ 1.76 ]
 % <- source.matlab meta.variable.other.valid.matlab
 % ^ source.matlab keyword.operator.symbols.matlab
-%   ^ source.matlab punctuation.section.brackets.begin.matlab 
-%     ^ source.matlab meta.brackets.matlab constant.numeric.matlab  
+%   ^ source.matlab punctuation.section.brackets.begin.matlab
+%     ^ source.matlab meta.brackets.matlab constant.numeric.matlab
 %          ^ source.matlab punctuation.section.brackets.end.matlab
 
 
@@ -50,9 +51,10 @@ xAprox = fMetodoDeNewton( xi )
 %  <- source.matlab meta.variable.other.valid.matlab
 %      ^ source.matlab keyword.operator.symbols.matlab
 %        ^ source.matlab meta.variable.other.valid.matlab
-%                       ^ source.matlab punctuation.section.parens.begin.matlab 
+%                       ^ source.matlab punctuation.section.parens.begin.matlab
 %                         ^ source.matlab meta.parens.matlab meta.variable.other.valid.matlab
-%                            ^ source.matlab punctuation.section.parens.end.matlab 
+%                            ^ source.matlab punctuation.section.parens.end.matlab
+
 
 
 
@@ -60,20 +62,38 @@ xAprox = fMetodoDeNewton( xi )
 % Block comment test
 
 % Success case
-%{           
+%{
 x = 5
 % ^ source.matlab comment.block.percentage.matlab
-%}           
+%}
 
-% Failure case
+
+% Failure cases for opening `%{`
 %{           fail
 x = 5
 % ^ source.matlab keyword.operator.symbols.matlab
 %}
 
+fail = 5 %{
+x = 5
+% ^ source.matlab keyword.operator.symbols.matlab
+%}
+
+
+% Failure cases for closing `%}`
 %{
 %}           fail
 x = 5
 % ^ source.matlab comment.block.percentage.matlab
 %}
+
+%{
+fail = 5 %}
+x = 5
+% ^ source.matlab comment.block.percentage.matlab
+%}
+
+
+
+
 
