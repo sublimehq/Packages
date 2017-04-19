9739fab9f4d2cbd4bc63bfdaf611cd7eec22ae30
Keith Hall <kingkeith+github@gmail.com>
2017-04-19 10:38:11 +0300

    [C#] show constructor params in local symbol list

--- C#/syntax_test_c#.cs
+++ C#/syntax_test_c#.cs
@@ -3,6 +3,20 @@
 class X
 // ^ storage.type.class
 {
+    X () {
+//  ^ entity.name.function.constructor
+//  ^^^^ meta.method
+    }
+
+    X (string test) : base () {
+//  ^ entity.name.function.constructor
+//  ^^^^^^^^^^^^^^^^^^^^^^^^^ meta.method.constructor
+//  ^^^^^^^^^^^^^^^ meta.method.constructor - meta.method.constructor.prebody
+//                    ^^^^ variable.language
+//                  ^^^^^^^^^^ meta.method.constructor.prebody
+//                         ^ meta.group punctuation.section.group.begin
+//                          ^ meta.group punctuation.section.group.end
+    }
 
     [Usage("Foo bar")]
 //  ^^^^^^^^^^^^^^^^^^ meta.annotation
@@ -28,7 +42,7 @@ class A
 {
    public A(int x, int y) {}
    // ^ storage.modifier
-   //     ^ entity.name.function
+   //     ^ entity.name.function.constructor
 }
 class B: A
 {
@@ -153,4 +167,12 @@ class Syntax
     //                     ^^ storage.type.numeric
     public ulong bad = 1UU;
     //                   ^ - storage.type.numeric
+}
+
+class Car
+{
+    ~Car() /* test */ // destructor
+//  ^^^^ entity.name.function.destructor
+    {
+    }
 }
