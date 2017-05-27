64bf21bccf23475b56ca5c1a450cced0f6e614f9
FichteFoll <fichtefoll2@googlemail.com>
2017-05-27 02:04:52 +0200

    [Python] Allow generators inside f-strings
    They really don't make sense semantically, since the result will be a
    stringified generator, but they are valid syntax.

--- Python/syntax_test_python_strings.py
+++ Python/syntax_test_python_strings.py
@@ -570,6 +570,9 @@ f'   \
 #^^^^^^^^^^^^^^ meta.string.interpolated
 # ^^^^^ source source.python.embedded
 
+f"{d for d in range(10)}"  # yes, this doesn't make sense
+#    ^^^ keyword.control.flow.for.generator.python
+
 f'
 # ^ invalid.illegal.unclosed-string
 
