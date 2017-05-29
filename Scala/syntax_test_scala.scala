fabb1e77fdbcf57de42bd6d507d0ae1a01d0c26a
Daniel Spiewak <djspiewak@gmail.com>
2017-05-29 14:49:44 -0600

    [Scala] Support for highlighting literals in types

--- Scala/syntax_test_scala.scala
+++ Scala/syntax_test_scala.scala
@@ -1631,3 +1631,21 @@ new RangeColumn(range) with LongColumn { def apply(row: Int) = a + row }
    type Bar = Unit
 //      ^^^ entity.name.type.scala
 //          ^ keyword.operator.assignment.scala
+
+type Foo = 42
+//         ^^ constant.numeric.integer.scala
+
+type Foo = "foo"
+//         ^^^^^ string.quoted.double.scala
+
+type Foo = 'foo
+//         ^^^^ constant.other.symbol.scala
+
+type Foo = 'a'
+//         ^^^ constant.character.literal.scala
+
+type Foo = true
+//         ^^^^ constant.language.scala
+
+type Foo = null
+//         ^^^^ constant.language.scala
