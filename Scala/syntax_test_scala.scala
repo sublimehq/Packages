444ebe16c177167b1bc7067b2c2474d7f3cac3bb
Daniel Spiewak <djspiewak@gmail.com>
2017-05-29 12:45:56 -0600

    [Scala] Removed type lambda trickery (Fixes #682)

--- Scala/syntax_test_scala.scala
+++ Scala/syntax_test_scala.scala
@@ -421,19 +421,6 @@ type Foo = Bar[A] forSome { type A }
    override
 // ^^^^^^^^ storage.modifier.other
 
-   val t: ({ type λ[α] = Foo[α] })#λ
-//        ^^^^^^^^^^^^^^ comment.block.scala
-//                       ^^^ support.class
-//                           ^ comment.block.empty.scala
-//                              ^^^^ comment.block.scala
-
-   val t: ({ type λ[α, β] = Foo[α, β] })#λ
-//        ^^^^^^^^^^^^^^^^^ comment.block.scala
-//                          ^^^ support.class
-//                              ^ comment.block.empty.scala
-//                                 ^ comment.block.empty.scala
-//                                    ^^^^ comment.block.scala
-
    a :: b :: Nil
 // ^^^^^^^^^ source.scala
 //           ^^^ support.constant.scala
@@ -724,11 +711,6 @@ type Foo = Foo.Bar
 
 type Foo = Foo#Bar
 //            ^ punctuation.accessor.scala
-
-val x: OptionT[({ type λ[α] = Foo[α, Int] })#λ, String] = ???
-//             ^^^^^^^^^^^^^^ comment.block
-//                                ^ comment.block.empty
-//                                        ^^^^ comment.block
 
 class Foo[+A]
 //        ^ keyword.operator
