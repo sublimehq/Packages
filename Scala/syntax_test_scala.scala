ad082baf94c7cb1ee5b060ef824d4c4c3b67a331
Daniel Spiewak <djspiewak@gmail.com>
2017-09-23 10:52:38 -0600

    [Scala] Bail out of accidental XML tag delcaration

--- Scala/syntax_test_scala.scala
+++ Scala/syntax_test_scala.scala
@@ -1631,3 +1631,9 @@ new RangeColumn(range) with LongColumn { def apply(row: Int) = a + row }
    type Bar = Unit
 //      ^^^ entity.name.type.scala
 //          ^ keyword.operator.assignment.scala
+
+   for {
+       _ <_
+   } yield true
+//   ^^^^^ keyword.control.flow.scala
+//         ^^^^ constant.language.scala
