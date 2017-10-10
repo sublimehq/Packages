971d8cbe685ca5c31fae097a70c2f2d1037dbd66
Keith Hall <kingkeith+github@gmail.com>
2017-10-10 13:31:02 +0300

    [C#] C#7 local functions - fix for methods with generic return types

--- C#/tests/syntax_test_C#7.cs
+++ C#/tests/syntax_test_C#7.cs
@@ -202,5 +202,36 @@ class Foo {
 ///                                     ^^^^^^^^ variable.other
     }
 /// ^ meta.class.body meta.method.body punctuation.section.block.end
+
+    // https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-7#local-functions
+    public Task<string> PerformLongRunningWork(string address, int index, string name)
+    {
+        if (string.IsNullOrWhiteSpace(address))
+            throw new ArgumentException(message: "An address is required", paramName: nameof(address));
+        if (index < 0)
+            throw new ArgumentOutOfRangeException(paramName: nameof(index), message: "The index must be non-negative");
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException(message: "You must supply a name", paramName: nameof(name));
+
+        return longRunningWorkImplementation();
+
+        async Task<string> longRunningWorkImplementation ()
+///     ^^^^^ storage.modifier
+///           ^^^^ support.type
+///               ^ punctuation.definition.generic.begin
+///                ^^^^^^ storage.type
+///                      ^ punctuation.definition.generic.end
+///                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ entity.name.function
+///                                                      ^^ meta.method.parameters
+        {
+///     ^ punctuation.section.block.begin
+///      ^ meta.class.body meta.method.body meta.method.body
+            var interimResult = await FirstWork(address);
+            var secondResult = await SecondStep(index, name);
+            return $"The results are {interimResult} and {secondResult}. Enjoy.";
+        }
+///     ^ punctuation.section.block.end
+///      ^ meta.class.body meta.method.body - meta.method.body meta.method.body
+    }
 }
 /// <- meta.class.body punctuation.section.block.end
