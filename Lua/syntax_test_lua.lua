8646a500c99d879d7f1cb2d3ff04d90c246e93c1
Resike <resike@gmail.com>
2018-03-14 20:49:20 +0100

    [Lua] adding syntax test file

---
+++ Lua/syntax_test_lua.lua
@@ -0,0 +1,85 @@
+-- SYNTAX TEST "Lua.sublime-syntax"
+
+local function foo(arg1, arg2, ...)
+--^^^ source.lua keyword.control.lua
+    --^^^^^^^^ source.lua keyword.control.lua
+             --^^^ source.lua meta.function.lua entity.name.function.lua
+                --^ source.lua meta.function.lua punctuation.definition.parameters.begin.lua
+                 --^^^^^^^^^^^^^^^ source.lua meta.function.lua variable.parameter.function.lua
+                                --^ source.lua meta.function.lua punctuation.definition.parameters.end.lua
+
+end
+--^ source.lua keyword.control.lua
+
+function foo(arg1, arg2, ...)
+--^^^^^^ source.lua keyword.control.lua
+       --^^^ source.lua meta.function.lua entity.name.function.lua
+          --^ source.lua meta.function.lua punctuation.definition.parameters.begin.lua
+           --^^^^^^^^^^^^^^^ source.lua meta.function.lua variable.parameter.function.lua
+                          --^ source.lua meta.function.lua punctuation.definition.parameters.end.lua
+
+end
+--^ source.lua keyword.control.lua
+
+function foo:bar(arg1, arg2, ...)
+--^^^^^^ keyword.control.lua
+       --^^^ source.lua meta.function.lua entity.name.function.scope.lua
+          --^ source.lua meta.function.lua entity.name.function.scope.lua
+           --^^^ source.lua meta.function.lua entity.name.function.lua
+              --^ source.lua meta.function.lua punctuation.definition.parameters.begin.lua
+               --^^^^^^^^^^^^^^^ source.lua meta.function.lua variable.parameter.function.lua
+                              --^ source.lua meta.function.lua punctuation.definition.parameters.end.lua
+
+end
+--^ source.lua keyword.control.lua
+
+function foo.bar(arg1, arg2, ...)
+--^^^^^^ keyword.control.lua
+       --^^^ source.lua meta.function.lua entity.name.function.scope.lua
+          --^ source.lua meta.function.lua entity.name.function.scope.lua
+           --^^^ source.lua meta.function.lua entity.name.function.lua
+              --^ source.lua meta.function.lua punctuation.definition.parameters.begin.lua
+               --^^^^^^^^^^^^^^^ source.lua meta.function.lua variable.parameter.function.lua
+                              --^ source.lua meta.function.lua punctuation.definition.parameters.end.lua
+
+end
+--^ source.lua keyword.control.lua
+
+function foo.bar:foo(arg1, arg2, ...)
+--^^^^^^ keyword.control.lua
+       --^^^ source.lua meta.function.lua entity.name.function.scope.lua
+          --^ source.lua meta.function.lua entity.name.function.scope.lua
+           --^^^ source.lua meta.function.lua entity.name.function.scope.lua
+              --^ source.lua meta.function.lua entity.name.function.scope.lua
+               --^^^ source.lua meta.function.lua entity.name.function.lua
+                  --^ source.lua meta.function.lua punctuation.definition.parameters.begin.lua
+                   --^^^^^^^^^^^^^^^ source.lua meta.function.lua variable.parameter.function.lua
+                                  --^ source.lua meta.function.lua punctuation.definition.parameters.end.lua
+
+end
+--^ source.lua keyword.control.lua
+
+function foo.bar.foo(arg1, arg2, ...)
+--^^^^^^ keyword.control.lua
+       --^^^ source.lua meta.function.lua entity.name.function.scope.lua
+          --^ source.lua meta.function.lua entity.name.function.scope.lua
+           --^^^ source.lua meta.function.lua entity.name.function.scope.lua
+              --^ source.lua meta.function.lua entity.name.function.scope.lua
+               --^^^ source.lua meta.function.lua entity.name.function.lua
+                  --^ source.lua meta.function.lua punctuation.definition.parameters.begin.lua
+                   --^^^^^^^^^^^^^^^ source.lua meta.function.lua variable.parameter.function.lua
+                                  --^ source.lua meta.function.lua punctuation.definition.parameters.end.lua
+
+end
+--^ source.lua keyword.control.lua
+
+self = function(arg1, arg2, ...)
+--^^ source.lua variable.language.self.lua
+   --^ source.lua keyword.operator
+     --^^^^^^^^ source.lua meta.function.lua keyword.control.lua
+             --^ source.lua meta.function.lua punctuation.definition.parameters.begin.lua
+              --^^^^^^^^^^^^^^^ source.lua meta.function.lua variable.parameter.function.lua
+                             --^ source.lua meta.function.lua punctuation.definition.parameters.end.lua
+
+end
+--^ source.lua keyword.control.lua
