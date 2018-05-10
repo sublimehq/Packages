d594e8fa1cd026ef68ce7238751dd7d79b7d5a0d
Raoul Wols <raoulwols@gmail.com>
2018-05-10 18:11:25 +0200

    [C++/ObjC] Fix: '-->' character sequence was not recognized as '--' and '>'

--- Objective-C/syntax_test_objc++.mm
+++ Objective-C/syntax_test_objc++.mm
@@ -1,4 +1,14 @@
 /* SYNTAX TEST "Packages/Objective-C/Objective-C++.sublime-syntax" */
+
+int main(){
+    int a=5,b=0;
+    while(a-->0)++b;
+    /*     ^^ keyword.operator.arithmetic */
+    /*       ^ keyword.operator.comparison */
+    /*        ^ constant.numeric */
+    /*          ^^ keyword.operator.arithmetic */
+}
+
 /////////////////////////////////////////////
 // Preprocessor
 /////////////////////////////////////////////
