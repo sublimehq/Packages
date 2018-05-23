713f046af5e36d399bba7a0647fd5056182e2fcb
Raoul Wols <raoulwols@gmail.com>
2018-05-23 14:14:59 +0200

    [ShellScript] Fix: forgot prototype context in old Shell-Unix-Generic syntax

---
+++ ShellScript/test/syntax_test_shell_unix_generic.sh
@@ -0,0 +1,10 @@
+# SYNTAX TEST "Packages/ShellScript/Shell-Unix-Generic.sublime-syntax"
+
+[[ ]]
+# <- support.function.double-brace.begin
+ # <- support.function.double-brace.begin
+ # ^^ support.function.double-brace.end
+
+ # make sure the prototype is included
+ # <- comment.line.number-sign punctuation.definition.comment.begin
+ #^^^^^^^^ comment.line.number-sign
