263990f56df919a3dc278e4e14dc7caa8b52fece
DeathAxe <deathaxe@googlemail.com>
2019-02-07 22:58:52 +0100

    [R] Use keyword.declaration.function
    This commit scopes the keyword `function` as `keyword.declaration.function`.

--- R/syntax_test_r.R
+++ R/syntax_test_r.R
@@ -144,7 +144,7 @@ f = function(x, y){ }
 #^^^^^^^^^^^ meta.function.r - meta.function.parameters.r
 # <- entity.name.function.r
 # ^ keyword.operator.assignment.r
-#   ^^^^^^^^ keyword.control.r
+#   ^^^^^^^^ keyword.declaration.function.r
 #           ^ punctuation.section.parameters.begin.r
 #            ^^^^ meta.function.parameters.r - meta.function.r
 #            ^ variable.parameter.r
@@ -167,7 +167,7 @@ foo(200, x = function(x) {x + y})
 #   ^^^ constant.numeric.float.decimal.r
 #        ^ variable.parameter.r
 #          ^ keyword.operator.assignment.r
-#            ^^^^^^^^ meta.function.r - meta.function.parameters.r
+#            ^^^^^^^^ meta.function.r keyword.declaration.function.r - meta.function.parameters.r
 #                    ^^^ meta.function.parameters.r
 #                               ^ punctuation.section.arguments.end.r
 
