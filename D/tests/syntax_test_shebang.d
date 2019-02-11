24167072d85b47275f8781e3958a73c6d1088e07
Benjamin Schaaf <ben.schaaf@gmail.com>
2019-02-11 14:59:11 +1100

    Replace shebang single regex with a simple context

--- D/tests/syntax_test_shebang.d
+++ D/tests/syntax_test_shebang.d
@@ -1,6 +1,6 @@
 #! SYNTAX TEST "Packages/D/D.sublime-syntax"
 #! <- comment.line.number-sign.d punctuation.definition.comment.number-sign
- #! <- comment.line.number-sign.d
+ #! <- comment.line.number-sign.d punctuation.definition.comment.number-sign
 #!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.line.number-sign.d
 
 // foo
