66e924d77477d1ac97ed1ed790d7a2458632a383
FichteFoll <fichtefoll2@googlemail.com>
2019-02-15 13:12:08 +0100

    [SQL] Add USING keyword

--- SQL/syntax_test_sql.sql
+++ SQL/syntax_test_sql.sql
@@ -13,6 +13,8 @@ SELECT "My "" Crazy Column Name" FROM my_table;
 SELECT
 (
 SELECT CASE field
+USING a
+-- <- keword.other.DML
     WHEN 1
     THEN -- comment's say that
 --                    ^ comment.line.double-dash
