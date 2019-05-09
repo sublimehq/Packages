4684165fc49f27cb28f05942bac30ae314f1e333
Jack Cherng <jfcherng@gmail.com>
2019-05-09 22:37:58 +0800

    [PHP] Fix scopes for logical operators
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- PHP/syntax_test_php.php
+++ PHP/syntax_test_php.php
@@ -1045,8 +1045,14 @@ $a += .5;
 $a .= 1;
 // ^^ keyword.operator.assignment.augmented.php
 
-if ($a !== $b);
-//     ^^^ keyword.operator.comparison.php
+if ($a && $b || !$c);
+//     ^^ keyword.operator.logical
+//           ^^ keyword.operator.logical
+//              ^ keyword.operator.logical
+
+if ($a !== $b || $a == $b);
+//     ^^^ keyword.operator.comparison
+//                  ^^ keyword.operator.comparison
 
 if ():
 else:
