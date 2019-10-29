cf3fd4b3437ca392ada497067d89ca91a0ee3251
DeathAxe <deathaxe@googlemail.com>
2019-10-29 19:22:18 +0100

    [Python] Simplify numeric suffix scope
    This commit applies a review request.
    
    Suffixes of numeric literals don't need to be too specific as ...
    
    1. they apply to single characters only
    2. should all be colored in the same way, especially if some literals
       have multiple ones.

--- Python/syntax_test_python.py
+++ Python/syntax_test_python.py
@@ -1192,10 +1192,10 @@ raise KeyError() from z
 decimal = 1234567890 + 9876543210L + -1 + -42L * 0000
 #         ^^^^^^^^^^ constant.numeric.integer.decimal.python
 #                      ^^^^^^^^^^^ constant.numeric.integer.decimal.python
-#                                ^ storage.type.numeric.long.python
+#                                ^ storage.type.numeric.python
 #                                    ^ keyword.operator.arithmetic.python - constant.numeric
 #                                         ^ keyword.operator.arithmetic.python - constant.numeric
-#                                            ^ storage.type.numeric.long.python
+#                                            ^ storage.type.numeric.python
 #                                                ^^^^ constant.numeric.integer
 
 floating = 0.1 - .1 * 10e-20 - 0.0e2 % 2.
@@ -1213,12 +1213,12 @@ binary = 0b1010011 | 0b0110110L
 #        ^^ punctuation.definition.numeric.binary.python
 #                    ^^^^^^^^^^ constant.numeric.integer.binary.python
 #                    ^^ punctuation.definition.numeric.binary.python
-#                             ^ storage.type.numeric.long.python
+#                             ^ storage.type.numeric.python
 
 octal = 0o755 ^ 0o644L
 #       ^^^^^ constant.numeric.integer.octal.python
 #       ^^ punctuation.definition.numeric.octal.python
-#                    ^ storage.type.numeric.long.python
+#                    ^ storage.type.numeric.python
 #               ^^^^^^ constant.numeric.integer.octal.python
 #               ^^ punctuation.definition.integer.octal.python
 
@@ -1229,14 +1229,14 @@ old_style_octal = 010 + 007 - 012345670L
 #                       ^ punctuation.definition.numeric.octal.python
 #                             ^^^^^^^^^^ constant.numeric.integer.octal.python
 #                             ^ punctuation.definition.integer.octal.python
-#                                      ^ storage.type.numeric.long.python
+#                                      ^ storage.type.numeric.python
 
 hexadecimal = 0x100af - 0XDEADF00L
 #             ^^^^^^^ constant.numeric.integer.hexadecimal.python
 #             ^^ punctuation.definition.numeric.hexadecimal.python
 #                       ^^^^^^^^^^ constant.numeric.integer.hexadecimal.python
 #                       ^^ punctuation.definition.numeric.hexadecimal.python
-#                                ^ storage.type.numeric.long.python
+#                                ^ storage.type.numeric.python
 
 unintuitive = 0B101 + 0O101 + 10l
 #             ^^^^^ constant.numeric.integer.binary.python
@@ -1244,7 +1244,7 @@ unintuitive = 0B101 + 0O101 + 10l
 #                     ^^^^^ constant.numeric.integer.octal.python
 #                     ^^ punctuation.definition.numeric.octal.python
 #                             ^^^ constant.numeric.integer.decimal.python
-#                               ^ storage.type.numeric.long.python
+#                               ^ storage.type.numeric.python
 
 illegal = 1LL << 08 | 0b010203 | 0xAbraCadabra
 #           ^ - constant.numeric
@@ -1262,8 +1262,8 @@ very_complex = 23_2.2e2_0J + 2_1j
 #              ^^^^^^^^^^^ constant.numeric.imaginary.decimal.python
 #                  ^ punctuation.separator.decimal.python
 #                            ^^^^ constant.numeric.imaginary.decimal.python
-#                        ^ storage.type.numeric.imaginary.python
-#                               ^ storage.type.numeric.imaginary.python
+#                        ^ storage.type.numeric.python
+#                               ^ storage.type.numeric.python
 
 addr = 0xCAFE_F00D
 #      ^^^^^^^^^^^ constant.numeric
