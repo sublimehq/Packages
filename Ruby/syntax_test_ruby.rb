ace8f213858db91cb12f4ba22b8e779597eceb4e
jwortmann <jwortmann@outlook.com>
2019-10-31 12:04:47 +0100

    [Ruby] Improve numbers (#2135)
    * [Ruby] Improve numbers
      * Fix rational and complex numbers (#2126)
      * Fix numbers with decimal or octal prefix
      * Fix dot in e.g. 123. being scoped as punctuation.accessor
      * Apply more specific scopes according to the guidelines
    * [Ruby] Change unnecessary capturing groups into non-capturing groups
    * [Ruby] Add punctuation.definition.numeric scope to number prefixes
    * [Ruby] Add suffix scopes for rational and imaginary numbers
    * [Ruby] Add scope for decimal point in numbers and allow 'r' and 'i' suffixes
    * [Ruby] Add more test cases for imaginary numbers
    * [Ruby] Extend rational/imaginary test cases
    * [Ruby] Introduce digit and exponent variables for better readability
    * [Ruby] Improve rational number rules
    * [Ruby] Improve floating number rules
    * [Ruby] Extend usage of digits variable
    * [Ruby] Correct the use of dot after integer as indicator for method call
    * [Ruby] Rewrite regex patterns to match all valid number notations
    * [Ruby] Add 'decimal' to integer and float scopes
    * [Ruby] Adjust scope names for imaginary numbers and suffixes

--- Ruby/syntax_test_ruby.rb
+++ Ruby/syntax_test_ruby.rb
@@ -181,6 +181,197 @@ BAR
 #  ^ - meta.string - string.unquoted
 
 ##################
+# Numbers
+##################
+
+ 1234
+#^^^^ constant.numeric.integer.decimal
+ 1_234
+#^^^^^ constant.numeric.integer.decimal
+ 0d170
+#^^^^^ constant.numeric.integer.decimal
+#^^ punctuation.definition.numeric.decimal
+ 0D170
+#^^^^^ constant.numeric.integer.decimal
+#^^ punctuation.definition.numeric.decimal
+ 0xAa
+#^^^^ constant.numeric.integer.hexadecimal
+#^^ punctuation.definition.numeric.hexadecimal
+ 0XAa
+#^^^^ constant.numeric.integer.hexadecimal
+#^^ punctuation.definition.numeric.hexadecimal
+ 0252
+#^^^^ constant.numeric.integer.octal
+#^ punctuation.definition.numeric.octal
+ 0o252
+#^^^^^ constant.numeric.integer.octal
+#^^ punctuation.definition.numeric.octal
+ 0O252
+#^^^^^ constant.numeric.integer.octal
+#^^ punctuation.definition.numeric.octal
+ 0b10101010
+#^^^^^^^^^^ constant.numeric.integer.binary
+#^^ punctuation.definition.numeric.binary
+ 0B10101010
+#^^^^^^^^^^ constant.numeric.integer.binary
+#^^ punctuation.definition.numeric.binary
+ 12.
+#^^ constant.numeric.integer.decimal
+#  ^ punctuation.accessor - constant.numeric - invalid.illegal
+ 12.ir
+#^^ constant.numeric.integer.decimal
+#  ^ punctuation.accessor - constant.numeric - invalid.illegal
+#   ^^ - constant.numeric - invalid.illegal - storage.type.numeric
+
+ 12.34
+#^^^^^ constant.numeric.float.decimal
+#  ^ punctuation.separator.decimal
+ 1234e-2
+#^^^^^^^ constant.numeric.float.decimal
+ 1.234E1
+#^^^^^^^ constant.numeric.float.decimal
+# ^ punctuation.separator.decimal
+ 12e3r
+#^^^^^ constant.numeric.float.decimal
+#    ^ invalid.illegal.numeric
+ 1.2e3r
+#^^^^^^ constant.numeric.float.decimal
+# ^ punctuation.separator.decimal
+#     ^ invalid.illegal.numeric
+
+ 12r
+#^^^ constant.numeric.rational.decimal
+#  ^ storage.type.numeric
+ 12.3r
+#^^^^^ constant.numeric.rational.decimal
+#  ^ punctuation.separator.decimal
+#    ^ storage.type.numeric
+ 0d170r
+#^^^^^^ constant.numeric.rational.decimal
+#^^ punctuation.definition.numeric.decimal
+#     ^ storage.type.numeric
+ 0xAar
+#^^^^^ constant.numeric.rational.hexadecimal
+#^^ punctuation.definition.numeric.hexadecimal
+#    ^ storage.type.numeric
+ 0o252r
+#^^^^^^ constant.numeric.rational.octal
+#^^ punctuation.definition.numeric.octal
+#     ^ storage.type.numeric
+ 0b10101010r
+#^^^^^^^^^^^ constant.numeric.rational.binary
+#^^ punctuation.definition.numeric.binary
+#          ^ storage.type.numeric
+
+ 12i
+#^^^ constant.numeric.imaginary.decimal
+#  ^ storage.type.numeric
+ 12.3i
+#^^^^^ constant.numeric.imaginary.decimal
+#  ^ punctuation.separator.decimal
+#    ^ storage.type.numeric
+ 12e3i
+#^^^^^ constant.numeric.imaginary.decimal
+#    ^ storage.type.numeric
+ 12e-3i
+#^^^^^^ constant.numeric.imaginary.decimal
+#     ^ storage.type.numeric
+ 1.2e3i
+#^^^^^^ constant.numeric.imaginary.decimal
+# ^ punctuation.separator.decimal
+#     ^ storage.type.numeric
+ 1.2e-3i
+#^^^^^^^ constant.numeric.imaginary.decimal
+# ^ punctuation.separator.decimal
+#      ^ storage.type.numeric
+ 12ri
+#^^^^ constant.numeric.imaginary.decimal
+#  ^^ storage.type.numeric
+ 12.3ri
+#^^^^^^ constant.numeric.imaginary.decimal
+#  ^ punctuation.separator.decimal
+#    ^^ storage.type.numeric
+ 0d170i
+#^^^^^^ constant.numeric.imaginary.decimal
+#^^ punctuation.definition.numeric.decimal
+#     ^ storage.type.numeric
+ 0d170ri
+#^^^^^^^ constant.numeric.imaginary.decimal
+#^^ punctuation.definition.numeric.decimal
+#     ^^ storage.type.numeric
+ 0xAai
+#^^^^^ constant.numeric.imaginary.hexadecimal
+#^^ punctuation.definition.numeric.hexadecimal
+#    ^ storage.type.numeric
+ 0xAari
+#^^^^^^ constant.numeric.imaginary.hexadecimal
+#^^ punctuation.definition.numeric.hexadecimal
+#    ^^ storage.type.numeric
+ 0o252i
+#^^^^^^ constant.numeric.imaginary.octal
+#^^ punctuation.definition.numeric.octal
+#     ^ storage.type.numeric
+ 0o252ri
+#^^^^^^^ constant.numeric.imaginary.octal
+#^^ punctuation.definition.numeric.octal
+#     ^^ storage.type.numeric
+ 0b10101010i
+#^^^^^^^^^^^ constant.numeric.imaginary.binary
+#^^ punctuation.definition.numeric.binary
+#          ^ storage.type.numeric
+ 0b10101010ri
+#^^^^^^^^^^^^ constant.numeric.imaginary.binary
+#^^ punctuation.definition.numeric.binary
+#          ^^ storage.type.numeric
+ 12e3ri
+#^^^^^^ constant.numeric.imaginary.decimal
+#    ^ invalid.illegal.numeric
+#     ^ storage.type.numeric 
+ 1.2e3ri
+#^^^^^^^ constant.numeric.imaginary.decimal
+# ^ punctuation.separator.decimal
+#     ^ invalid.illegal.numeric
+#      ^ storage.type.numeric
+ 12ir
+#^^^^ constant.numeric.imaginary.decimal
+#  ^ storage.type.numeric
+#   ^ invalid.illegal.numeric
+ 12.3ir
+#^^^^^^ constant.numeric.imaginary.decimal
+#  ^ punctuation.separator.decimal
+#    ^ storage.type.numeric
+#     ^ invalid.illegal.numeric
+ 12e3ir
+#^^^^^^ constant.numeric.imaginary.decimal
+#    ^ storage.type.numeric
+#     ^ invalid.illegal.numeric
+ 1.2e3ir
+#^^^^^^^ constant.numeric.imaginary.decimal
+# ^ punctuation.separator.decimal
+#     ^ storage.type.numeric
+#      ^ invalid.illegal.numeric
+ 0d170ir
+#^^^^^^^ constant.numeric.imaginary.decimal
+#^^ punctuation.definition.numeric.decimal
+#     ^ storage.type.numeric
+#      ^ invalid.illegal.numeric
+ 0xAair
+#^^^^^^ constant.numeric.imaginary.hexadecimal
+#^^ punctuation.definition.numeric.hexadecimal
+#    ^ storage.type.numeric
+#     ^ invalid.illegal.numeric
+ 0o252ir
+#^^^^^^^ constant.numeric.imaginary.octal
+#^^ punctuation.definition.numeric.octal
+#     ^ storage.type.numeric
+#      ^ invalid.illegal.numeric
+ 0b10101010ir
+#^^^^^^^^^^^^ constant.numeric.imaginary.binary
+#^^ punctuation.definition.numeric.binary
+#          ^ storage.type.numeric
+#           ^ invalid.illegal.numeric
+
+##################
 # Strings
 ##################
 
@@ -518,7 +709,7 @@ Symbol === :foo
 #       ^^ constant.character.ruby
 #         ^ - constant
 #          ^ keyword.operator.conditional.ruby
-#           ^^ constant.numeric.ruby
+#           ^^ constant.numeric.integer.decimal.ruby
   ?a ?A ?あ ?abc ?a0
 #^ - constant
 # ^ punctuation.definition.constant.ruby
