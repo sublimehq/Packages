935c4b74b3fadbc60d4494cf0a053f86b005d48c
DeathAxe <deathaxe82@googlemail.com>
2020-08-08 12:43:40 +0200

    [ShellScript] Move variables tests before variables-interpolations

--- ShellScript/test/syntax_test_bash.sh
+++ ShellScript/test/syntax_test_bash.sh
@@ -931,133 +931,6 @@ foo[${j}+10]="`foo`"
 
 
 ####################################################################
-# Variables                                                        #
-####################################################################
-
-: $__
-# ^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
-# ^ punctuation.definition.variable.shell
-#    ^ - meta.interpolation - variable
-
-: $var_0
-# ^^^^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
-# ^ punctuation.definition.variable.shell
-#       ^ - meta.interpolation - variable
-
-: $_var0
-# ^^^^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
-# ^ punctuation.definition.variable.shell
-#       ^ - meta.interpolation - variable
-
-: $_0var_
-# ^^^^^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
-# ^ punctuation.definition.variable.shell
-#        ^ - meta.interpolation - variable
-
-# Expands to the positional parameters, starting from one. When the expansion is
-# not within double quotes, each positional parameter expands to a separate
-# word. In contexts where it is performed, those words are subject to further
-# word splitting and pathname expansion. When the expansion occurs within double
-# quotes, it expands to a single word with the value of each parameter separated
-# by the first character of the IFS special variable. That is, "$*" is
-# equivalent to "$1c$2c…", where c is the first character of the value of the
-# IFS variable. If IFS is unset, the parameters are separated by spaces. If IFS
-# is null, the parameters are joined without intervening separators.
-: $*
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-# Expands to the positional parameters, starting from one. When the expansion
-# occurs within double quotes, each parameter expands to a separate word. That
-# is, "$@" is equivalent to "$1" "$2" …. If the double-quoted expansion occurs
-# within a word, the expansion of the first parameter is joined with the
-# beginning part of the original word, and the expansion of the last parameter
-# is joined with the last part of the original word. When there are no
-# positional parameters, "$@" and $@ expand to nothing (i.e., they are removed).
-: $@
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-# Expands to the number of positional parameters in decimal.
-: $#
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-# Expands to the exit status of the most recently executed foreground pipeline.
-: $?
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-# Expands to the current option flags as specified upon invocation, by the set
-# builtin command, or those set by the shell itself (such as the -i option).
-: $-
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-# Expands to the process ID of the shell. In a () subshell, it expands to the
-# process ID of the invoking shell, not the subshell.
-: $$
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-# Expands to the process ID of the job most recently placed into the background,
-# whether executed as an asynchronous command or using the bg builtin
-: $!
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-# Expands to the name of the shell or shell script. This is set at shell
-# initialization. If Bash is invoked with a file of commands (see Shell
-# Scripts), $0 is set to the name of that file. If Bash is started with the -c
-# option (see Invoking Bash), then $0 is set to the first argument after the
-# string to be executed, if one is present. Otherwise, it is set to the filename
-# used to invoke Bash, as given by argument zero.
-: $0
-# ^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
-# ^ punctuation.definition.variable.shell
-
-# At shell startup, set to the absolute pathname used to invoke the shell or
-# shell script being executed as passed in the environment or argument list.
-# Subsequently, expands to the last argument to the previous command, after
-# expansion. Also set to the full pathname used to invoke each command executed
-# and placed in the environment exported to that command. When checking mail,
-# this parameter holds the name of the mail file.
-: $_
-# ^^ meta.interpolation.parameter.shell variable.language.shell
-# ^ punctuation.definition.variable.shell
-
-fg %
-#  ^ meta.interpolation.job.shell variable.other.readwrite.shell punctuation.definition.variable.shell
-fg %%
-#  ^^ meta.interpolation.job.shell variable.language.job.shell
-#  ^ punctuation.definition.variable.shell
-fg %+
-#  ^^ meta.interpolation.job.shell variable.language.job.shell
-#  ^ punctuation.definition.variable.shell
-fg %-
-#  ^^ meta.interpolation.job.shell variable.language.job.shell
-#  ^ punctuation.definition.variable.shell
-fg %1 %2 %3
-#  ^^ meta.interpolation.job.shell variable.language.job.shell
-#  ^ punctuation.definition.variable.shell
-#     ^^ meta.interpolation.job.shell variable.language.job.shell
-#     ^ punctuation.definition.variable.shell
-#        ^^ meta.interpolation.job.shell variable.language.job.shell
-#        ^ punctuation.definition.variable.shell
-fg %ce
-#  ^^^ meta.interpolation.job.shell variable.other.readwrite.shell
-#  ^ punctuation.definition.variable.shell
-fg %?ce
-#  ^^^^ meta.interpolation.job.shell variable.other.readwrite.shell
-#  ^ punctuation.definition.variable.shell
-#   ^ keyword.operator.regexp.quantifier.shell
-
-%1
-# <- meta.interpolation.job.shell variable.language.job.shell punctuation.definition.variable.shell
-#^ meta.interpolation.job.shell variable.language.job.shell
-
-
-####################################################################
 # 4.2 Bash Builtin Commands                                        #
 ####################################################################
 
@@ -1421,6 +1294,133 @@ sudo --reset-timestamp -n -- rm -rf
 #                            ^^ variable.function.shell
 #                               ^^^ meta.parameter.option.shell variable.parameter.option.shell
 #                               ^ punctuation.definition.parameter.shell
+
+
+####################################################################
+# Variables                                                        #
+####################################################################
+
+: $__
+# ^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
+# ^ punctuation.definition.variable.shell
+#    ^ - meta.interpolation - variable
+
+: $var_0
+# ^^^^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
+# ^ punctuation.definition.variable.shell
+#       ^ - meta.interpolation - variable
+
+: $_var0
+# ^^^^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
+# ^ punctuation.definition.variable.shell
+#       ^ - meta.interpolation - variable
+
+: $_0var_
+# ^^^^^^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
+# ^ punctuation.definition.variable.shell
+#        ^ - meta.interpolation - variable
+
+# Expands to the positional parameters, starting from one. When the expansion is
+# not within double quotes, each positional parameter expands to a separate
+# word. In contexts where it is performed, those words are subject to further
+# word splitting and pathname expansion. When the expansion occurs within double
+# quotes, it expands to a single word with the value of each parameter separated
+# by the first character of the IFS special variable. That is, "$*" is
+# equivalent to "$1c$2c…", where c is the first character of the value of the
+# IFS variable. If IFS is unset, the parameters are separated by spaces. If IFS
+# is null, the parameters are joined without intervening separators.
+: $*
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+# Expands to the positional parameters, starting from one. When the expansion
+# occurs within double quotes, each parameter expands to a separate word. That
+# is, "$@" is equivalent to "$1" "$2" …. If the double-quoted expansion occurs
+# within a word, the expansion of the first parameter is joined with the
+# beginning part of the original word, and the expansion of the last parameter
+# is joined with the last part of the original word. When there are no
+# positional parameters, "$@" and $@ expand to nothing (i.e., they are removed).
+: $@
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+# Expands to the number of positional parameters in decimal.
+: $#
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+# Expands to the exit status of the most recently executed foreground pipeline.
+: $?
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+# Expands to the current option flags as specified upon invocation, by the set
+# builtin command, or those set by the shell itself (such as the -i option).
+: $-
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+# Expands to the process ID of the shell. In a () subshell, it expands to the
+# process ID of the invoking shell, not the subshell.
+: $$
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+# Expands to the process ID of the job most recently placed into the background,
+# whether executed as an asynchronous command or using the bg builtin
+: $!
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+# Expands to the name of the shell or shell script. This is set at shell
+# initialization. If Bash is invoked with a file of commands (see Shell
+# Scripts), $0 is set to the name of that file. If Bash is started with the -c
+# option (see Invoking Bash), then $0 is set to the first argument after the
+# string to be executed, if one is present. Otherwise, it is set to the filename
+# used to invoke Bash, as given by argument zero.
+: $0
+# ^^ meta.interpolation.parameter.shell variable.other.readwrite.shell
+# ^ punctuation.definition.variable.shell
+
+# At shell startup, set to the absolute pathname used to invoke the shell or
+# shell script being executed as passed in the environment or argument list.
+# Subsequently, expands to the last argument to the previous command, after
+# expansion. Also set to the full pathname used to invoke each command executed
+# and placed in the environment exported to that command. When checking mail,
+# this parameter holds the name of the mail file.
+: $_
+# ^^ meta.interpolation.parameter.shell variable.language.shell
+# ^ punctuation.definition.variable.shell
+
+fg %
+#  ^ meta.interpolation.job.shell variable.other.readwrite.shell punctuation.definition.variable.shell
+fg %%
+#  ^^ meta.interpolation.job.shell variable.language.job.shell
+#  ^ punctuation.definition.variable.shell
+fg %+
+#  ^^ meta.interpolation.job.shell variable.language.job.shell
+#  ^ punctuation.definition.variable.shell
+fg %-
+#  ^^ meta.interpolation.job.shell variable.language.job.shell
+#  ^ punctuation.definition.variable.shell
+fg %1 %2 %3
+#  ^^ meta.interpolation.job.shell variable.language.job.shell
+#  ^ punctuation.definition.variable.shell
+#     ^^ meta.interpolation.job.shell variable.language.job.shell
+#     ^ punctuation.definition.variable.shell
+#        ^^ meta.interpolation.job.shell variable.language.job.shell
+#        ^ punctuation.definition.variable.shell
+fg %ce
+#  ^^^ meta.interpolation.job.shell variable.other.readwrite.shell
+#  ^ punctuation.definition.variable.shell
+fg %?ce
+#  ^^^^ meta.interpolation.job.shell variable.other.readwrite.shell
+#  ^ punctuation.definition.variable.shell
+#   ^ keyword.operator.regexp.quantifier.shell
+
+%1
+# <- meta.interpolation.job.shell variable.language.job.shell punctuation.definition.variable.shell
+#^ meta.interpolation.job.shell variable.language.job.shell
 
 
 ####################################################################
