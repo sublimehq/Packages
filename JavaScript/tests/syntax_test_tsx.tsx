ec091f8f5c3ab68c1474f47f686799b46ac199a8
wbond <will@wbond.net>
2020-09-18 15:34:36 -0400

    [JavaScript] Rename TypeScript to TSX, add proper TypeScript syntax

---
+++ JavaScript/tests/syntax_test_tsx.tsx
@@ -0,0 +1,25 @@
+// SYNTAX TEST "Packages/JavaScript/TSX.sublime-syntax"
+
+    <foo />;
+//  ^^^^^^^ meta.jsx meta.tag
+//  ^ punctuation.definition.tag.begin
+//   ^^^ meta.tag.name entity.name.tag
+//       ^^ punctuation.definition.tag.end
+
+    type x < T = Foo > = any;
+//  ^^^^^^^^^^^^^^^^^^^^^^^^ meta.type-alias
+//  ^^^^ storage.type
+//       ^ entity.name.type
+//         ^^^^^^^^^^^ meta.generic
+//         ^ punctuation.definition.generic.begin
+//           ^ variable.parameter.generic
+//             ^ keyword.operator.assignment
+//               ^^^ support.class
+//                   ^ punctuation.definition.generic.end
+//                     ^ keyword.operator.assignment
+//                       ^^^ meta.type-alias support.type.any
+
+// This is invalid TSX as the TypeScript type assertion is parsed as a JSX tag
+let strLength: number = (<string>someValue).length; // </string>
+//                       ^^^^^^^^ meta.tag - meta.assertion
+//                                                     ^^^^^^^^^ meta.tag - comment
