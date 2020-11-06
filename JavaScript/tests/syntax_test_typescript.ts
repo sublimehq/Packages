ef7f2f808fd6b08c101ad694df7bd2716f6b0e60
Thom Smith <thom@thomthesmith.com>
2020-11-06 11:46:08 -0500

    [TypeScript] Revert improved scoping of anonymous arrow function names.

--- JavaScript/tests/syntax_test_typescript.ts
+++ JavaScript/tests/syntax_test_typescript.ts
@@ -379,9 +379,6 @@ function f(this : any) {}
 //                   ^ keyword.operator.ternary
 //                     ^ variable.other.readwrite
 
-    const f = (): T => 42;
-//        ^ meta.function.declaration entity.name.function variable.other.readwrite
-
 /* Assertions */
 
 x as boolean;
