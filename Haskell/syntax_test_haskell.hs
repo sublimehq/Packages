f6e6c8d8ac32bdba4c542c41273f563ae8d93891
DeathAxe <deathaxe82@googlemail.com>
2020-12-22 20:43:39 +0100

    [Haskell] Fix comments
    Fixes #2670
    
    According to https://www.haskell.org/onlinereport/haskell2010/haskellch10.html
    a comment starts with at least 2 dashes followed by `any` character but
    symbols with some exceptions. The relevant rules are:
    
      comment   -> -- {-} [ any⟨symbol⟩ {any} ] newline
      symbol    -> ascSymbol | uniSymbol⟨special|_|"|'⟩
      special   -> ( | ) | , | ; | [ | ] | ` | { | }
      ascSymbol -> ! | # | $ | % | & | ⋆ | + | . | / | < | = | > | ? | @ | \
                 | ^ | | | - | ~ | :
      uniSymbol -> any Unicode symbol or punctuation
    
    With:
    
      any       -> graphic | space | tab
      graphic   -> small | large | symbol | digit | special | " | '
    
    we can say:
    
      comment   -> -- {-} [
                   space | tab | small | large | digit | special | " | '
                   {any} ] newline
    
    This simplified pattern is implemented in this commit.
    
    Additionally `(--)` and `(---)` are excluded from infix operators.

--- Haskell/syntax_test_haskell.hs
+++ Haskell/syntax_test_haskell.hs
@@ -42,6 +42,85 @@
 --                      ^^ punctuation.definition.comment.end.haskell
 --                        ^ - comment.block.haskell
 
+
+-- COMMENTS STARTING WITH SPECIAL SYMBOL CHARS
+
+   --
+-- ^^^ comment
+   --_
+-- ^^^ comment
+   --"
+-- ^^^ comment
+   --'
+-- ^^^ comment
+   --(
+-- ^^^ comment
+   --)
+-- ^^^ comment
+   --,
+-- ^^^ comment
+   ---
+-- ^^^ comment
+   --;
+-- ^^^ comment
+   --[
+-- ^^^ comment
+   --]
+-- ^^^ comment
+   --`
+-- ^^^ comment
+   --{
+-- ^^^ comment
+   --}
+-- ^^^ comment
+
+
+-- NO COMMENTS
+
+   --!
+-- ^^^ - comment
+   --#
+-- ^^^ - comment
+   --$
+-- ^^^ - comment
+   --%
+-- ^^^ - comment
+   --&
+-- ^^^ - comment
+   --*
+-- ^^^ - comment
+   --+
+-- ^^^ - comment
+   --.
+-- ^^^ - comment
+   --.
+-- ^^^ - comment
+   --/
+-- ^^^ - comment
+   --:
+-- ^^^ - comment
+   --<
+-- ^^^ - comment
+   --=
+-- ^^^ - comment
+   -->
+-- ^^^ - comment
+   --?
+-- ^^^ - comment
+   --\
+-- ^^^ - comment
+   --\
+-- ^^^ - comment
+   --^
+-- ^^^ - comment
+   --|
+-- ^^^ - comment
+   --~
+-- ^^^ - comment
+   --~
+-- ^^^ - comment
+
+
 --DECLARATIONS
 
    module Name where
@@ -138,6 +217,21 @@
 --       ^^^ variable.function.infix.haskell
 --             ^ meta.number.integer.decimal.haskell constant.numeric.value.haskell
 
+   a a = (--) a 2
+--     ^ keyword.operator.haskell
+--       ^^^^ - variable.function.infix
+--       ^ punctuation.section.group.begin.haskell
+--        ^^^^^^^^ comment.line.double-dash.haskell
+         )
+--       ^ punctuation.section.group.end.haskell
+
+   a a = (---) a 2
+--     ^ keyword.operator.haskell
+--       ^^^^^ - variable.function.infix
+--       ^ punctuation.section.group.begin.haskell
+--        ^^^^^^^^^ comment.line.double-dash.haskell
+         )
+--       ^ punctuation.section.group.end.haskell
 
    a `member` x
 --   ^^^^^^^^ keyword.operator.function.infix.haskell
