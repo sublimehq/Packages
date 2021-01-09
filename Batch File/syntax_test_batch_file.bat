cd956609b3826e7434c4436a05f950077296831f
DeathAxe <deathaxe82@googlemail.com>
2021-01-09 19:19:23 +0100

    [Batch File] Add TITLE command contexts

--- Batch File/syntax_test_batch_file.bat
+++ Batch File/syntax_test_batch_file.bat
@@ -2432,3 +2432,35 @@ ECHO : Not a comment ^
 ::               ^ punctuation.definition.prompt.end.dosbatch
 ::                ^ keyword.operator.redirection.dosbatch
 ::                 ^^^^^^^^^^^^^^^^^^^^^^^^^ string.quoted.double.dosbatch
+
+
+:::: [ TITLE ] ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+   title
+:: ^^^^^ meta.command.title.dosbatch support.function.builtin.dosbatch
+::      ^ - meta.command
+
+   title Foo 123.5 bar
+:: ^^^^^^ meta.command.title.dosbatch
+::       ^^^^^^^^^^^^^ meta.command.title.arguments.dosbatch meta.string.dosbatch
+::                    ^ - meta.command - meta.string
+:: ^^^^^ support.function.builtin.dosbatch
+::       ^^^^^^^^^^^^^ string.unquoted.dosbatch - constant
+::                    ^ - string
+
+   title:Foo
+:: ^^^^^ meta.command.title.dosbatch
+::      ^^^^ meta.command.title.arguments.dosbatch meta.string.dosbatch
+::          ^ - meta.command - meta.string
+:: ^^^^^ support.function.builtin.dosbatch
+::      ^^^^ string.unquoted.dosbatch - constant
+::          ^ - string
+
+   title /? ignored
+:: ^^^^^ meta.command.title.dosbatch
+::      ^^^ meta.command.title.arguments.dosbatch
+::         ^^^^^^^^^ - meta.command
+:: ^^^^^ support.function.builtin.dosbatch
+::       ^ variable.parameter.help.dosbatch punctuation.definition.variable.dosbatch
+::        ^ variable.parameter.help.dosbatch - punctuation
+::          ^^^^^^^ comment.line.ignored.dosbatch
