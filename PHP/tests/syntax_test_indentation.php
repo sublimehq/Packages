a4d7c5091990a8fbec76e0b9d8156f6fcafca492
Keith Hall <keith-hall@users.noreply.github.com>
2021-03-10 18:26:34 +0200

    [PHP] add some more indentation tests (#2712)

--- PHP/tests/syntax_test_indentation.php
+++ PHP/tests/syntax_test_indentation.php
@@ -5,6 +5,8 @@ https://github.com/sublimehq/Packages/issues/1924
     asdfadf
 <?php elseif(false): ?>
     asdf
+<?php else: ?>
+    qwfpg
 <?php endif;?>
 
 <?php
@@ -55,4 +57,33 @@ function test()
         ok;
     }
 }
+
+for (expr1; expr2; expr3)
+    statement;
+
+for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+}
+
+for ($i = 1; $i <= 10; $i++)
+{
+    echo $i;
+}
+
+for ($i = 1, $j = 0; $i <= 10; $j += $i, print $i, $i++);
+
+$people = array(
+    array('name' => 'Kalle', 'salt' => 856412),
+    array('name' => 'Pierre', 'salt' => 215863)
+);
+
+for($i = 0, $size = count($people); $i < $size; ++$i) {
+    $people[$i]['salt'] = mt_rand(000000, 999999);
+}
+
+for (expr1; expr2; expr3):
+    statement;
+    statement;
+endfor;
+
 ?>
