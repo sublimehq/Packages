ea2d74d0c52c2ec35186550f81067ee98cf7324c
DeathAxe <deathaxe82@googlemail.com>
2021-09-05 12:30:56 +0200

    [Java] Tweak block comment patterns

--- Java/tests/syntax_test_java.java
+++ Java/tests/syntax_test_java.java
@@ -19,6 +19,18 @@
 // ^^^^^^^^^ comment.line.double-slash.java - punctuation
 //          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.line.double-slash.java punctuation.definition.comment.java
 
+   /**/ /***/ /****/ /* **/
+//^ - comment
+// ^^^^ comment.block.empty.java punctuation.definition.comment.java
+//     ^ - comment
+//      ^^^^^ comment.block.empty.java punctuation.definition.comment.java
+//           ^ - comment
+//            ^^^^^^ comment.block.empty.java punctuation.definition.comment.java
+//                   ^^ comment.block.java punctuation.definition.comment.begin.java
+//                     ^ comment.block.java - punctuation
+//                      ^^^ comment.block.java punctuation.definition.comment.end.java
+//                         ^ - comment
+
    /*
 //^ - comment
 // ^^ comment.block.java punctuation.definition.comment.begin.java
