c6eecfe73e9bf05fecd3d5ac66595c47442877f2
Keith Hall <kingkeith+github@gmail.com>
2021-09-19 23:11:14 +0300

    [SQL] add TSQL tests, cleanup

--- SQL/syntax_test_tsql.sql
+++ SQL/syntax_test_tsql.sql
@@ -180,3 +180,139 @@ SET @Blah = CASE @x WHEN 2 THEN 'Y' WHEN @z THEN 'N' ELSE NULL END
 --                                                        ^^^^ constant.language.null
 --                                                             ^^^ keyword.control.conditional.end
 --                                                                ^ - meta
+
+
+
+DECLARE @name SYSNAME -- database name
+-- ^^^^ keyword.declaration.variable
+--      ^ punctuation.definition.variable
+--       ^^^^ variable.other.readwrite
+--            ^^^^^^^ storage.type
+--                    ^^ comment.line.double-dash punctuation.definition.comment
+DECLARE @path NVARCHAR(256) -- path for backup files
+-- ^^^^ keyword.declaration.variable
+--      ^ variable.other.readwrite punctuation.definition.variable
+--       ^^^^ variable.other.readwrite
+--            ^^^^^^^^^^^^^ storage.type
+--                     ^^^ constant.numeric
+DECLARE @fileName NVARCHAR(256) -- filename for backup
+DECLARE @fileDate VARCHAR(20) -- used for file name
+
+SET @path = 'C:\Backup\'
+
+SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112)
+--               ^ keyword.operator
+--                 ^^^^^^^ meta.function-call support.function.scalar
+--                        ^ meta.function-call meta.group punctuation.section.parens.begin
+--                         ^^^^^^^^^^^ storage.type
+--                                    ^ punctuation.separator
+--                                     ^^^^^^^ support.function.scalar
+--                                            ^ punctuation.section.parens.begin
+--                                             ^ punctuation.section.parens.end
+--                                              ^ punctuation.separator.argument
+--                                               ^^^ meta.number.integer.decimal constant.numeric.value
+--                                                  ^ punctuation.section.parens.end
+--                                                   ^ - meta.function-call - meta.group
+
+DECLARE db_cursor CURSOR FOR
+-- ^^^^ keyword.declaration.variable
+--                ^^^^^^^^^^ keyword.other
+
+SELECT name
+FROM MASTER.dbo.sysdatabases
+WHERE name NOT IN ('master','model','msdb','tempdb')
+
+OPEN db_cursor
+-- ^ keyword.other
+FETCH NEXT FROM db_cursor INTO @name
+-- ^^^^^^^ keyword.other
+--         ^^^^ keyword.other
+--                        ^^^^ keyword.other
+--                             ^^^^^ variable.other.readwrite
+
+WHILE @@FETCH_STATUS = 0
+-- ^^ keyword.control.flow
+--    ^^^^^^^^^^^^^^ support.variable.global
+--    ^^ support.variable.global
+--                   ^ keyword.operator.comparison
+--                     ^ meta.number.integer.decimal constant.numeric.value
+BEGIN
+-- ^^ keyword.control.flow.begin
+      SET @fileName = @path + @name + '_' + @fileDate + '.BAK'
+      BACKUP DATABASE @name TO DISK = @fileName
+
+      FETCH NEXT FROM db_cursor INTO @name
+END
+-- <- keyword.control.flow.end
+
+CLOSE db_cursor
+-- ^^ keyword.other
+DEALLOCATE db_cursor
+-- ^^^^^^^ keyword.other
+
+-------------
+
+DECLARE @FileExists INT
+SET NOCOUNT ON
+--          ^^ keyword
+EXEC master.dbo.xp_fileexist @FromFile, @FileExists OUTPUT
+-- ^ keyword.control.flow
+--                                                  ^^^^^^ keyword.other
+SET NOCOUNT OFF
+--          ^^^ keyword.other
+IF @FileExists = 0
+BEGIN
+    RAISERROR ('File "%s" does not exist', 16, -1, @FromFile)
+    RETURN -1
+    -- ^^^ keyword.control.flow
+    --     ^ keyword.operator.arithmetic
+    --      ^ meta.number.integer.decimal constant.numeric.value
+END
+
+SET @Message = 'Importing data from file...'
+RAISERROR (@Message, 0, 1) WITH NOWAIT
+-- ^^^^^^ meta.function-call support.function
+--                         ^^^^ keyword.other
+--                              ^^^^^^ keyword.other
+
+SELECT COALESCE(a.field1, b.field2, c.field1) AS Blah, ISNULL(d.field1, 'default') as field1
+--     ^^^^^^^^ meta.function-call support.function
+--                                            ^^ keyword.operator.assignment.alias
+--                                                     ^^^^^^ meta.function-call support.function
+-------------------------------
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'every 10 seconds',
+        @enabled=1,
+        @freq_type=4,
+        @freq_interval=1,
+        @freq_subday_type=2,
+        @freq_subday_interval=10,
+        @freq_relative_interval=0,
+        @freq_recurrence_factor=0,
+        @active_start_date=20150713,
+        @active_end_date=99991231,
+        @active_start_time=0,
+        @active_end_time=235959,
+        @schedule_uid=N'564354f8-4985-7408-80b7-afdc2bb92d3c'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+--                                    ^^^^ keyword.control.flow
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+-- ^^^^^^^^^^^^^^^ keyword.context
+GOTO EndSave
+QuitWithRollback:
+-- ^^^^^^^^^^^^^ entity.name.label
+--              ^ punctuation.definition.label
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+    --  ^^^^^^^^^^^ meta.group support.variable.global
+    --              ^ keyword.operator.comparison
+EndSave:
+
+-------------
+
+INSERT INTO my_table (foo, bar)
+VALUES (2, 'two'),
+       (3, 'three')
+
+
+-- merge, CTEs
