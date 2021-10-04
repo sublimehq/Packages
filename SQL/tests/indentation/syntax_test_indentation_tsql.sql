2227515c1c6bf593633cdb8695953c02dcfa6559
Keith Hall <kingkeith+github@gmail.com>
2021-10-04 21:31:53 +0300

    [SQL] reorganize syntax tests into subfolders

---
+++ SQL/tests/indentation/syntax_test_indentation_tsql.sql
@@ -0,0 +1,65 @@
+-- SYNTAX TEST reindent "Packages/SQL/TSQL.sublime-syntax"
+SELECT *
+FROM (
+    SELECT *
+    FROM a
+    INNER JOIN b ON a.ID = b.ID
+) alias
+WHERE 1 = 1
+GO
+
+BEGIN
+    INSERT INTO a
+    VALUES (123, 456)
+END
+
+BEGIN
+    INSERT INTO a
+    VALUES (123, 456)
+GO
+    INSERT INTO b
+    VALUES (123, 456)
+END
+
+SELECT
+a,
+b,
+c
+FROM foo
+
+SELECT a
+,b
+,c
+FROM foo
+
+CREATE TABLE hello (
+    --       @@@@@ definition
+    field1 int,
+    field2 nvarchar(50)
+)
+
+CREATE UNIQUE NONCLUSTERED INDEX IX_some_index ON dbo.some_table(
+    --                           @@@@@@@@@@@@@ definition
+    some_column ASC
+)
+
+SET @fileDate = CONVERT(VARCHAR(20),GETDATE(),112)
+--              @@@@@@@ reference
+--                                  @@@@@@@ reference
+
+BEGIN  RAISERROR ('File "%s" does not exist', 16, -1, @FromFile)
+    -- @@@@@@@@@ reference
+END
+
+SELECT COALESCE(a.field1, b.field2, c.field1) AS Blah, ISNULL(d.field1, 'default') as field1
+--     @@@@@@@@ reference
+--                                                     @@@@@@ reference
+
+'no indentation increase when an unclosed open paren is in a string('
+BEGIN
+    'no indentation decrease when a close paren with no matching open paren on the same line is in a string)'
+    null
+    -- no indentation increase when an unclosed open paren is in a comment (
+    --no indentation decrease when a close paren with no matching open paren on the same line is in a comment )
+    null
+END
