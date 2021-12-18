108f6941a7222bb9a385689fd14ff037f1b00291
Keith Hall <keith-hall@users.noreply.github.com>
2021-12-18 06:43:26 +0200

    [C#] Improve pattern matching support for C#10 (#3169)
    * [C#] Improve pattern matching support for C#10
    
    * [C#] Support pattern matching on anonymous types

--- C#/tests/syntax_test_C#10.cs
+++ C#/tests/syntax_test_C#10.cs
@@ -44,3 +44,61 @@ public readonly record struct Person(string Name);
 ///                                         ^^^^ variable.parameter
 ///                                             ^ punctuation.section.parameters.end
 ///                                              ^ punctuation.terminator.statement
+
+if (obj is Person { Address: { City: "Seattle" } })
+///     ^^ keyword.operator.reflection
+///        ^^^^^^ support.type
+///               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.instance.property-subpattern meta.class.body.anonymous
+///                 ^^^^^^^ variable.other.member
+///                        ^ keyword.operator.assignment
+///                          ^ punctuation.section.block.begin
+///                            ^^^^ variable.other.member
+///                                ^ keyword.operator.assignment
+///                                  ^^^^^^^^^ string.quoted.double
+///                                            ^ punctuation.section.block.end
+///                                              ^ punctuation.section.block.end
+    Console.WriteLine("Seattle");
+
+if (obj is Person { Address.City: "Seattle" }) // Extended property pattern
+///     ^^ keyword.operator.reflection
+///        ^^^^^^ support.type
+///               ^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.instance.property-subpattern meta.class.body.anonymous
+///                 ^^^^^^^ variable.other.member
+///                        ^ punctuation.accessor.dot
+///                         ^^^^ variable.other.member
+///                             ^ keyword.operator.assignment
+///                               ^^^^^^^^^ string.quoted.double
+///                                         ^ punctuation.section.block.end
+    Console.WriteLine("Seattle");
+
+static bool IsConferenceDay(DateTime date) => date is { Year: 2020, Month: 5, Day: 19 or 20 or 21 };
+///^^^ storage.modifier
+///    ^^^^ storage.type
+///         ^^^^^^^^^^^^^^^ meta.method entity.name.function
+///                        ^ meta.method.parameters punctuation.section.parameters.begin
+///                         ^^^^^^^^ meta.method.parameters support.type
+///                                  ^^^^ meta.method.parameters variable.parameter
+///                                      ^ meta.method.parameters punctuation.section.parameters.end
+///                                        ^^ meta.method keyword.declaration.function.arrow
+///                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.method
+///                                           ^^^^ variable.other
+///                                                ^^ keyword.operator.reflection
+///                                                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.instance.property-subpattern meta.class.body.anonymous meta.block
+///                                                   ^ punctuation.section.block.begin
+///                                                     ^^^^ variable.other.member
+///                                                         ^ keyword.operator.assignment
+///                                                           ^^^^ meta.number.integer.decimal constant.numeric.value
+///                                                               ^ punctuation.separator.property
+///                                                                 ^^^^^ variable.other.member
+///                                                                      ^ keyword.operator.assignment
+///                                                                        ^ meta.number.integer.decimal constant.numeric.value
+///                                                                         ^ punctuation.separator.property
+///                                                                           ^^^ variable.other.member
+///                                                                              ^ keyword.operator.assignment
+///                                                                                ^^ meta.number.integer.decimal constant.numeric.value
+///                                                                                   ^^ keyword.operator.logical
+///                                                                                      ^^ meta.number.integer.decimal constant.numeric.value
+///                                                                                         ^^ keyword.operator.logical
+///                                                                                            ^^ meta.number.integer.decimal constant.numeric.value
+///                                                                                               ^ punctuation.section.block.end
+///                                                                                                ^ punctuation.terminator.statement
