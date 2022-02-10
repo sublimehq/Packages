5534f39aed80c65b289ad668448a277df32c0d70
DeathAxe <deathaxe82@googlemail.com>
2022-02-10 21:44:03 +0100

    [Haskell] Add shebang highlighting
    The keywords `cabal` and `stack` are borrowed from
    - https://github.com/haskell/haskell-language-server/blob/4b8f9d53bed3e775ccd120d9a2471ae374a88db3/install.hs#L1
    - https://github.com/haskell/haskell-language-server/blob/4b8f9d53bed3e775ccd120d9a2471ae374a88db3/GenChangelogs.hs#L1

---
+++ Haskell/tests/syntax_test_shebang.hs
@@ -0,0 +1,4 @@
+#!cabal SYNTAX TEST "Packages/Haskell/Haskell.sublime-syntax"
+#!cabal <- comment.line.shebang.haskell punctuation.definition.comment.haskell
+  #!cabal <- comment.line.shebang.haskell constant.language.shebang.haskell
+#!cabal^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.line.shebang.haskell
