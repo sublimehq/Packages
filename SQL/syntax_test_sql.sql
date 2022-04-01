af852320753fb7963f5f0e833a72bac84eb7f2cf
Aziz Köksal <aziz.koeksal@gmail.com>
2022-04-01 23:23:11 +0200

    [SQL] Better regexp for `timestamp(?:s|tz)`
    Don't assign storage.type.sql to whitespace.

--- SQL/syntax_test_sql.sql
+++ SQL/syntax_test_sql.sql
@@ -96,9 +96,13 @@ create table fancy_table (
     fancy_column character  varying(42) DEFAULT 'nice'::character varying,
 --               ^^^^^^^^^^^^^^^^^^ storage.type.sql
     mytime timestamp(3) without time zone DEFAULT now(),
---                      ^^^^^^^^^^^^^^^^^ storage.type.sql
+--                      ^^^^^^^ storage.type.sql
+--                              ^^^^ storage.type.sql
+--                                   ^^^^ storage.type.sql
     mytime2 timestamp(3) without  time  zone DEFAULT '2008-01-18 00:00:00'::timestamp(3) without time zone,
---                       ^^^^^^^^^^^^^^^^^^^ storage.type.sql
+--                       ^^^^^^^ storage.type.sql
+--                                ^^^^ storage.type.sql
+--                                      ^^^^ storage.type.sql
     primary key (id),
 --  ^^^^^^^^^^^ storage.modifier.sql
     UNIQUE (foreign_id),
