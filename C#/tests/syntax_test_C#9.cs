bedaf72195c1014fe3c5e1fa04f64737c3d1c453
deathaxe <deathaxe82@googlemail.com>
2022-10-20 18:10:10 +0200

    [C#] Fix nested record definitions (#3493)
    * [C#] Fix some statement terminator scopes
    
    Fixes a minor issue with some semi-colons still being scoped
    
       `punctuation.terminator.cs`
    
    while others use
    
       `punctuation.terminator.statement.cs`
    
    * [C#] Fix record definitions
    
    Fixes #3489
    
    This commit ...
    
    1. fixes `data_type_body` (now `data_type_expect_block`) to be popped
       off stack too early.
    2. fixes meta scopes of record parameter lists, which used to be
       `meta.method.parameters`. Now using `meta.class.record.parameters`.
    3. improves overall meta scope boundaries for record definitions.
    
    * [C#] Don't match generics after record parameters
    
    This commit introduces a little tweak aka. a bit stricter matching of
    generic <...> brackets to avoid them being matched/consumed 
    after record parameters as they are not valid in that position.

--- C#/tests/syntax_test_C#9.cs
+++ C#/tests/syntax_test_C#9.cs
@@ -301,16 +301,21 @@ if (e is not Customer) { }
 ///          ^^^^^^^^ support.type
 
 public record A(int Num);
-///    ^^^^^^^^^^^^^^^^^ meta.class
-///                     ^ punctuation.terminator.statement
-///           ^ meta.class.record entity.name.class
+///    ^^^^^^^^ meta.class.record
+///            ^^^^^^^^^ meta.class.record.parameters
+///                     ^ - meta.class
+///    ^^^^^^ keyword.declaration.class.record
+///           ^ entity.name.class
 ///            ^ punctuation.section.parameters.begin
-///             ^^^^^^^^ meta.method.parameters
 ///             ^^^ storage.type
 ///                 ^^^ variable.parameter
 ///                    ^ punctuation.section.parameters.end
-public record B<T>(T Num);
+///                     ^ punctuation.terminator.statement
+public record B<T>(T Num)<NoGeneric>;
 ///    ^^^^^^^^^^^ meta.class.record
+///               ^^^^^^^ meta.class.record.parameters
+///                      ^^^^^^^^^^^ meta.class.record - meta.generic
+///                                 ^ - meta.class
 ///    ^^^^^^ keyword.declaration.class.record
 ///           ^ entity.name.class
 ///            ^^^ meta.generic
@@ -318,10 +323,13 @@ public record B<T>(T Num);
 ///             ^ support.type
 ///              ^ punctuation.definition.generic.end
 ///               ^ punctuation.section.parameters.begin
-///                ^^^^^ meta.method.parameters
 ///                     ^ punctuation.section.parameters.end
-///                      ^ punctuation.terminator.statement
+///                                 ^ punctuation.terminator.statement
 public record C<TNum> (TNum Num) where TNum : class;
+///    ^^^^^^^^^^^^^^^ meta.class.record.cs
+///                   ^^^^^^^^^^ meta.class.record.parameters.cs
+///                             ^^^^^^^^^^^^^^^^^^^ meta.class.record.cs
+///                                                ^ - meta.class
 ///    ^^^^^^ keyword.declaration.class.record
 ///           ^ entity.name.class
 ///            ^ punctuation.definition.generic.begin
@@ -337,6 +345,10 @@ public record C<TNum> (TNum Num) where TNum : class;
 ///                                           ^^^^^ storage.type
 ///                                                ^ punctuation.terminator.statement
 public record D<TNum> (TNum Num) where TNum : class { public const int TEST = 4; }
+///    ^^^^^^^^^^^^^^^ meta.class.record.cs
+///                   ^^^^^^^^^^ meta.class.record.parameters.cs
+///                             ^^^^^^^^^^^^^^^^^^^^ meta.class.record.cs
+///                                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.class.record.body.cs meta.block.cs
 ///    ^^^^^^ keyword.declaration.class.record
 ///           ^ entity.name.class
 ///            ^ punctuation.definition.generic.begin
@@ -373,8 +385,40 @@ public record Person(
 ///                                                          ^ punctuation.separator.parameter.function
     [property: JsonPropertyName("lastName")]string LastName);
 /// ^ punctuation.definition.annotation.begin
-/// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.class.body meta.method.parameters meta.annotation
+/// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.class.record.parameters meta.annotation
 ///                                         ^^^^^^ storage.type
 ///                                                ^^^^^^^^ variable.parameter
 ///                                                        ^ punctuation.section.parameters.end
 ///                                                         ^ punctuation.terminator.statement
+
+public class MyClass { public record MyRecord <T> (int nums) { public const int TEST = 4; } }
+///^^^^ - meta.class
+///    ^^^^^^^^^^^^^^ meta.class - meta.class.body
+///                  ^^^^^^^^^ meta.class.body meta.block
+///                           ^^^^^^^^^^^^^^^^^^^^ meta.class.body meta.block meta.class.record - meta.class.record.parameters
+///                                               ^^^^^^^^^^ meta.class.body meta.block meta.class.record.parameters
+///                                                         ^ meta.class.body meta.block meta.class.record - meta.class.record.parameters
+///                                                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.class.body meta.block meta.class.record.body meta.block
+///                                                                                        ^^ meta.class.body meta.block - meta.class meta.class
+///                                                                                          ^ - meta.class
+///^^^ storage.modifier.access
+///    ^^^^^ keyword.declaration.class
+///          ^^^^^^^ entity.name.class
+///                  ^ punctuation.section.block.begin
+///                    ^^^^^^ storage.modifier.access
+///                           ^^^^^^ keyword.declaration.class.record
+///                                  ^^^^^^^^ entity.name.class
+///                                           ^ punctuation.definition.generic.begin
+///                                            ^ support.type
+///                                             ^ punctuation.definition.generic.end
+///                                               ^ meta.class.record.parameters punctuation.section.parameters.begin
+///                                                ^^^ storage.type
+///                                                    ^^^^ variable.parameter
+///                                                        ^ punctuation.section.parameters.end
+///                                                          ^ punctuation.section.block.begin
+///                                                            ^^^^^^ storage.modifier.access
+///                                                                   ^^^^^ storage.modifier
+///                                                                         ^^^ storage.type
+///                                                                             ^^^^ variable.other.member
+///                                                                                       ^ punctuation.section.block.end
+///                                                                                         ^ punctuation.section.block.end
