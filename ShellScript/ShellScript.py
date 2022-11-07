ffb7c174977b1f01bd8471623e3256a1c68614e0
Johannes Rappen <jrappen@users.noreply.github.com>
2022-11-07 16:06:27 +0100

    [ShellScript] add typing info

--- ShellScript/ShellScript.py
+++ ShellScript/ShellScript.py
@@ -1,41 +1,46 @@
+from __future__ import annotations
+
+import sublime
+import Default.exec
+
 import copy
 import os
 import re
 import shlex
-import Default.exec
+import typing
 
 
 class RunShellScriptCommand(Default.exec.ExecCommand):
 
     def run(self, **kwargs):
-        view = self.window.active_view()
-        if not view:
+        view: typing.Optional[sublime.View] = self.window.active_view()
+        if view is None:
             return
-        file_name = view.file_name()
-        if not file_name:
+        file_name: typing.Optional[str] = view.file_name()
+        if file_name is None:
             return
 
         # Determine the shell to run
-        firstline = view.substr(view.line(0))
-        match = re.match(r"\s*#!(.+)", firstline)
+        firstline: str = view.substr(view.line(0))
+        match: typing.Optional[re.Match[str]] = re.match(r"\s*#!(.+)", firstline)
         if match:
             # Note: we split on whitespace and turn it
             # into a list, otherwise things like
             # "/usr/bin/env bash" won't work
-            shell = shlex.split(match.group(1))
+            shell: typing.List[str] = shlex.split(match.group(1))
         else:
             # No shebang line... Take the user's default shell
-            shell = [os.environ["SHELL"]]
+            shell: typing.List[str] = [os.environ["SHELL"]]
 
         # Determine the working directory
         working_dir = kwargs.get("working_dir", None)
-        if not working_dir:
+        if working_dir is None:
             working_dir = os.path.dirname(file_name)
 
         # Determine the environment variables
         env = copy.deepcopy(os.environ)
         custom_env = kwargs.get("env", None)
-        if custom_env:
+        if custom_env is not None:
             env.update(custom_env)
 
         # Delegate to the super class
