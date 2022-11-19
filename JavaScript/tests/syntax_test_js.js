4c02b6a139c6944a7e1a7cbe585f912de99bdcf1
Thomas Smith <thom@thomthesmith.com>
2022-11-19 12:22:36 -0500

    [JavaScript] Use branching to improve continuation scopes. (#3257)
    When a decorator ends at the end of a line, end the annotation scope there. 
    This requires branching to look across the line boundary.
    
    Notes:
    
    Also reimplement expression statement line continuation in the same way. 
    This eliminates the old set/set back pattern in favor of branch/pop. 
    I was going to reuse this pattern in `variable-binding-list-top`, 
    but it turns out that the line continuation code there wasn't doing 
    anything in the first place, so I removed it.

--- JavaScript/tests/syntax_test_js.js
+++ JavaScript/tests/syntax_test_js.js
@@ -738,8 +738,27 @@ class MyClass extends TheirClass {
 //   ^^^ variable.annotation
 //      ^^^^^^^^^^^^ meta.function - meta.annotation
 
-    ['foo']() {}
+    @foo
+//  ^^^^ meta.annotation
+//      ^ - meta.annotation
+    bar() {}
+//  ^^^^^^^^ meta.function
+
+    @foo
+//  ^^^^ meta.annotation
+//      ^ - meta.annotation
+    ['bar']() {}
 //  ^^^^^^^^^^^^ meta.function
+
+    @foo()
+//  ^^^^^^ meta.annotation
+//        ^ - meta.annotation
+    bar() {}
+
+    @(foo)
+//  ^^^^^^ meta.annotation
+//        ^ - meta.annotation
+    bar() {}
 
     static ['foo']() {}
 //         ^^^^^^^^^^^^ meta.function
@@ -970,6 +989,7 @@ sources.DOM
 // <- variable.other.readwrite
     .status()
     // ^ meta.function-call.method variable.function
+    //       ^ - meta.function-call
 
     foo.#bar();
 //  ^^^^^^^^^^ meta.function-call.method.js
@@ -1024,7 +1044,7 @@ baz(foo(x => x('bar')));
 //                   ^ meta.function-call meta.function-call punctuation.section.group.end
 //                    ^ meta.function-call punctuation.section.group.end
 
-func(a, b);
+func(a, b) ;
 //^^^^^^^^ meta.function-call
 // ^ variable.function
 //  ^^^^^^ meta.group
@@ -1033,6 +1053,7 @@ func(a, b);
 //    ^ punctuation.separator.comma
 //      ^ variable.other.readwrite
 //       ^ punctuation.section.group.end
+//        ^ - meta.function-call
 
 var instance = new Constructor(param1, param2)
 //                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.function-call.constructor
