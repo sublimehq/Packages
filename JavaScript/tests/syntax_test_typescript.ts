1e6dfc11e3479ff2ca6c229bcd9394418115c98c
Thomas Smith <thom@thomthesmith.com>
2023-02-17 20:03:38 -0500

    [TypeScript] Implement satisfies operator (#3665)
    * [TypeScript] Implement satisfies operator
    
    * [TypeScript] Improve satisfies implementation

--- JavaScript/tests/syntax_test_typescript.ts
+++ JavaScript/tests/syntax_test_typescript.ts
@@ -716,6 +716,10 @@ x as boolean;
 //^^ keyword.operator.type
 //   ^^^^^^^ meta.type support.type.primitive.boolean
 
+x satisfies boolean;
+//^^^^^^^^^ keyword.operator.type
+//          ^^^^^^^ meta.type support.type.primitive.boolean
+
 x as const;
 //^^ keyword.operator.type
 //   ^^^^^ storage.modifier.const
