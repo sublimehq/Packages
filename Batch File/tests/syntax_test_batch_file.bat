9bae4518d6722eda5f38a6de8c60a2c42248651e
deathaxe <deathaxe82@googlemail.com>
2023-05-08 09:37:17 +0200

    [Batch File] Fix at operator highlighting (#3726)
    Fixes #3725

--- Batch File/tests/syntax_test_batch_file.bat
+++ Batch File/tests/syntax_test_batch_file.bat
@@ -104,13 +104,74 @@ ECHO : Not a comment ^
 :: <- - comment
 ::^^^^^^^^^^^^  - comment
 
+:::: [ @ Operator ] :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+   @
+:: ^ keyword.operator.at.dosbatch
+
+   @::comment
+:: ^ keyword.operator.at.dosbatch
+::  ^^^^^^^^^^ comment.line.colon.dosbatch
+
+   @:label
+:: ^ keyword.operator.at.dosbatch
+::  ^^^^^^ entity.name.label.dosbatch
+::  ^ punctuation.definition.label.dosbatch
+
+   @ :label
+:: ^ keyword.operator.at.dosbatch
+::   ^^^^^^ entity.name.label.dosbatch
+::   ^ punctuation.definition.label.dosbatch
+
+   @:@@@@@
+:: ^ keyword.operator.at.dosbatch
+::  ^^^^^^ entity.name.label.dosbatch
+::  ^ punctuation.definition.label.dosbatch
+
+   @ :@@@@@
+:: ^ keyword.operator.at.dosbatch
+::   ^^^^^^ entity.name.label.dosbatch
+::   ^ punctuation.definition.label.dosbatch
 
    @ECHO OFF
 :: ^ keyword.operator.at.dosbatch
+::  ^^^^ support.function.builtin.dosbatch
 
-   @
-:: ^ - keyword.operator.at.dosbatch
+   @ ECHO OFF
+:: ^ keyword.operator.at.dosbatch
+::   ^^^^ support.function.builtin.dosbatch
 
+   @ @ @@ ECHO OFF
+:: ^ keyword.operator.at.dosbatch
+::   ^ keyword.operator.at.dosbatch
+::     ^^ keyword.operator.at.dosbatch
+::        ^^^^ support.function.builtin.dosbatch
+
+   IF "%1"=="--debug" (@echo off) ELSE (@ @ GOTO :EOF)
+::                     ^ keyword.operator.at.dosbatch
+::                      ^^^^ support.function.builtin.dosbatch
+::                                      ^ keyword.operator.at.dosbatch
+::                                        ^ keyword.operator.at.dosbatch
+::                                          ^^^^ keyword.control.flow.goto.dosbatch
+
+   @(goto) 2>nul || @(title %ComSpec%)
+:: ^ keyword.operator.at.dosbatch
+::  ^^^^^^ meta.block.dosbatch
+::                  ^ keyword.operator.at.dosbatch
+::                   ^^^^^^^^^^^^^^^^^ meta.block.dosbatch
+
+   E@CHO john.doe@email.com
+::  ^ - keyword-operator
+::               ^ - keyword-operator
+
+   dir @logs
+::     ^ - keyword-operator
+
+   dir C:\@logs
+::        ^ - keyword-operator
+
+   my @
+::    ^ - keyword-operator
 
 :::: [ Labels ] :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
@@ -1479,9 +1540,6 @@ put arg1 arg2
 
 
 :::: [ ECHO Command ] :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-   @
-:: ^ - keyword.operator.at.dosbatch
 
    ECHO || ECHO && ECHO &
 :: ^^^^ support.function.builtin.dosbatch
