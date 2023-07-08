5dc0ffa0f1f7b2ffed2db1e6d59d7a4b943ac3ae
deathaxe <deathaxe82@googlemail.com>
2023-07-08 21:54:03 +0200

    Adjust syntax test

--- Batch File/tests/syntax_test_batch_file.bat
+++ Batch File/tests/syntax_test_batch_file.bat
@@ -1825,7 +1825,7 @@ put arg1 arg2
 ::                                           ^^^^^^ - constant
 ::                                                 ^^ constant.character.escape.dosbatch
 ::                                                   ^^^ - constant
-::                                                      ^ constant.other.placeholder.dosbatch
+::                                                      ^ variable.language.wildcard.asterisk.dosbatch
 ::                                                       ^^ constant.character.escape.dosbatch
 ::                                                         ^ - constant
 ::                                                          ^^ constant.character.escape.dosbatch
