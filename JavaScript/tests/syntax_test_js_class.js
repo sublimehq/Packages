4e10175aedba669348a4f93b874fa52d8e674af3
Thomas Smith <thom@thomthesmith.com>
2023-10-31 09:52:26 -0400

    [JavaScript] Clean up class member code (#3850)
    Inspired by improvements from #3848.
    
    Consolidate the code handling class members (fields and methods) into a more coherent and stack-based approach.
    
    - Handle “backtrackable” elements (modifiers, accessor keywords, async) in a self-contained way in the `method-declaration` stack.
    - As a side benefit of the above, remove the duplicate get/set/async code that was needed due to https://github.com/sublimehq/sublime_text/issues/3494.
    - Also expand function scopes to include modifiers.
    - Explicitly handle semicolons in class field declarations, letting us mark extraneous semicolons `punctuation.terminator.statement.empty.js` like in statement contexts. (This has proved useful for testing in the past.)
    - Remove `static-block-body`, which can be replaced with the regular `block` context.
    - Various minor improvements.
    
    Co-authored-by: deathaxe <deathaxe82@googlemail.com>

--- JavaScript/tests/syntax_test_js_class.js
+++ JavaScript/tests/syntax_test_js_class.js
@@ -11,6 +11,7 @@ class MyClass extends TheirClass {
 //  ^ variable.other.readwrite
 //    ^ keyword.operator.assignment
 //      ^^ constant.numeric
+//        ^ punctuation.terminator.statement - punctuation.terminator.statement.empty
 
     'y' = 42;
 //  ^^^ meta.string string.quoted.single
@@ -121,9 +122,18 @@ class MyClass extends TheirClass {
     static = 42;
 //  ^^^^^^ variable.other.readwrite
 
+    static static = 42;
+//  ^^^^^^ storage.modifier.js
+//         ^^^^^^ variable.other.readwrite
+
     static() {}
 //  ^^^^^^^^^^^ meta.function
 //  ^^^^^^ entity.name.function
+
+    static static() {}
+//  ^^^^^^^^^^^^^^^^^^ meta.function
+//  ^^^^^^ storage.modifier.js
+//         ^^^^^^ entity.name.function
 
     accessor foo;
 //  ^^^^^^^^ storage.modifier
@@ -142,7 +152,8 @@ class MyClass extends TheirClass {
         for (const param of this.#data.get('value')) {}
 //                               ^ punctuation.definition.variable
 //                                ^^^^ meta.property.object
-    }
+    };
+//   ^ punctuation.terminator.statement.empty.js
 
     #privateMethod() {}
 //  ^^^^^^^^^^^^^^^^^^^ meta.function
@@ -168,8 +179,13 @@ class MyClass extends TheirClass {
     }
 
     get *foo()
+//  ^^^^^^^^ meta.function
+//  ^^^ storage.type.accessor
+//      ^ keyword.generator.asterisk
+//       ^^^ entity.name.function
 
     static foo(baz) {
+//  ^^^^^^^^^^^^^^^^^ meta.function
 //  ^^^^^^ storage.modifier
 //         ^^^^^^^^^^ meta.function
     //     ^^^ entity.name.function
