b4d864f5493b38ede70348ad9fbb3fc3ca6f20f2
deathaxe <deathaxe82@googlemail.com>
2024-07-26 17:43:15 +0200

    [JavaScript] Handle function calls in mappings or lists
    This commit...
    
    1. extends selectors for each syntax (JS, JSX, TS, TSX) to choose correct
       indentation rules in nested mappings, lists or functions/lambdas.
    
    2. Adds check for trailing `:` to case/default indentation rule patterns,
       as a function called `case()` in a mapping can't be prevented from being
       unindented via selectors.

--- JavaScript/tests/syntax_test_indent.js
+++ JavaScript/tests/syntax_test_indent.js
@@ -1,5 +1,44 @@
 // SYNTAX TEST reindent "Packages/JavaScript/JavaScript.sublime-syntax"
 
+export default {
+    default: 'value',
+    case() {
+        const map1 = {
+            default: 'value'
+        }
+        const list1 = [
+            default
+        ]
+
+        if (foo == true)
+            return 1
+        else if (bar == true)
+            return 2
+        else
+            return 3
+
+        switch (map1) {
+        case null:
+            return 0
+        default:
+            const map2 = {
+                default: 'value'
+            }
+            const list2 = [
+                default,
+                () => {
+                    switch (map2) {
+                    default:
+                        return 0
+                    case null:
+                        return 1
+                    }
+                }
+            ]
+        }
+    }
+}
+
 /*
 mapping definitions
 */
