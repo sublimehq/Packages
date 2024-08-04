8ba9bf2ce38518ad888d13980a04f530523888fd
deathaxe <deathaxe82@googlemail.com>
2024-08-04 15:14:14 +0200

    [ShellScript] Fix symbol definitions

---
+++ ShellScript/Bash/tests/syntax_test_symbol.bash
@@ -0,0 +1,86 @@
+# SYNTAX TEST "Packages/ShellScript/Bash.sublime-syntax"
+
+###############################################################################
+# ALIAS symbols
+###############################################################################
+
+alias myabbr="git \"status\""
+#     @@@@@@ definition
+#             @@@ reference
+
+  myabbr
+# @@@@@@ reference
+
+( myabbr )
+# @@@@@@ reference
+
+{ myabbr }
+# @@@@@@ reference
+
+var=`myabbr`
+#    @@@@@@ reference
+
+  unalias myabbr
+# @@@@@@@ reference
+#         @@@@@@ reference
+
+###############################################################################
+# COPROC symbols
+###############################################################################
+
+coproc proc { func1 running }
+#      @@@@ definition
+#             @@@@@ reference
+
+###############################################################################
+# FUNCTION symbols
+###############################################################################
+
+function func1() { func2() }
+#        @@@@@ definition
+#                  @@@@@ definition
+
+function $fname()
+#        @@@@@@ definition
+
+function f$name()
+#        @@@@@@ definition
+
+function f${name:$start:5}()
+#        @@@@@@@@@@@@@@@@@ definition
+
+  func3()
+# @@@@@ definition
+
+  func3
+# @@@@@ reference
+
+  'func3'
+#  @@@@@ reference
+
+  "func3"
+#  @@@@@ reference
+
+( func3 )
+# @@@@@ reference
+
+{ func3 }
+# @@@@@ reference
+
+  trap 'func3' SIGTERM
+# @@@@ reference
+#       @@@@@ reference
+
+  trap "func3" SIGTERM
+# @@@@ reference
+#       @@@@@ reference
+
+  trap `func3` SIGTERM
+# @@@@ reference
+#       @@@@@ reference
+
+  /usr/bin/app arg
+# @@@@@@@@@@@@ reference
+
+  ~/.bin/**/app?[Nn]ame arg
+# @@@@@@@@@@@@@@@@@@@@@ reference
