49517f466679ce1bcd4c901f1ec91d5d168522d0
deathaxe <deathaxe82@googlemail.com>
2024-08-11 11:29:36 +0200

    [ShellScript] Add a common word-end context
    Handle word-termination and line continuation in common re-usable context.

--- ShellScript/Zsh/tests/syntax_test_scope.zsh
+++ ShellScript/Zsh/tests/syntax_test_scope.zsh
@@ -4634,6 +4634,19 @@ print ${arr//(#m)[aeiou]/${(U)MATCH}}
 #                                                          ^ punctuation.definition.quoted.end.shell.zsh
 #                                                           ^ punctuation.definition.modifier.end.shell.zsh
 
+: (/*(/:t)) # qualifiers are valid in groups
+# ^^^ meta.string.shell meta.group.regexp.shell string.unquoted.shell - meta.modifier
+#    ^^^^^ meta.string.shell meta.group.regexp.shell meta.modifier.glob.shell.zsh
+#         ^ meta.string.shell meta.group.regexp.shell string.unquoted.shell - meta.modifier
+# ^ meta.group.regexp.shell string.unquoted.shell punctuation.section.group.begin.regexp.shell
+#   ^ constant.other.wildcard.asterisk.shell
+#    ^ punctuation.definition.modifier.begin.shell.zsh
+#     ^ storage.modifier.glob.shell.zsh
+#      ^ punctuation.separator.sequence.shell.zsh
+#       ^ storage.modifier.glob.shell.zsh
+#        ^ punctuation.definition.modifier.end.shell.zsh
+#         ^ punctuation.section.group.end.regexp.shell
+
 ls /(D)/  # qualifiers are always located at the end
 #  ^^^^^ meta.string.shell string.unquoted.shell
 #   ^^^ meta.group.regexp.shell - meta.modifier
