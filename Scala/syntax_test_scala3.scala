893ba90ce5d69a82441013748334afa71601ceab
Daniel Spiewak <djspiewak@gmail.com>
2024-12-30 09:29:07 -0600

    [Scala] Added support for context function types and fixed scoping

--- Scala/syntax_test_scala3.scala
+++ Scala/syntax_test_scala3.scala
@@ -297,16 +297,19 @@ x: [A] =>> Foo[A]
 // ^ punctuation.definition.generic.begin.scala
 //  ^ support.class.scala
 //   ^ punctuation.definition.generic.end.scala
-//     ^^^ keyword.declaration.function.arrow.type-lambda.scala
+//     ^^^ keyword.operator.arrow.type-lambda.scala
 //         ^^^ support.class.scala
 
 x: [A, B[_], C] =>> Foo[A, B, C]
 // ^ punctuation.definition.generic.begin.scala
 //  ^ support.class.scala
 //       ^ variable.language.underscore.scala
-//              ^^^ keyword.declaration.function.arrow.type-lambda.scala
+//              ^^^ keyword.operator.arrow.type-lambda.scala
 //                  ^^^ support.class.scala
 
 type Y =
   [A] =>> Foo
-//    ^^^ keyword.declaration.function.arrow.type-lambda.scala
+//    ^^^ keyword.operator.arrow.type-lambda.scala
+
+type Executable[T] = ExecutionContext ?=> T
+//                                    ^^^ keyword.operator.arrow.type-context.scala
