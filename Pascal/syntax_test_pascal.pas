bdc22b693ece22ceeb6843893b147b9884ff9228
Michael Lyons <github@michael.lyo.nz>
2025-02-18 15:53:04 -0500

    [Pascal] Constant subscopes

--- Pascal/syntax_test_pascal.pas
+++ Pascal/syntax_test_pascal.pas
@@ -8,6 +8,13 @@
 //^^^^^^^^^^^^^^^^^^^^^^ comment.line.double-dash.pascal
 //^^ punctuation.definition.comment.begin.pascal
 
+  true
+//^^^^ constant.language.boolean.true.pascal
+  false
+//^^^^^ constant.language.boolean.false.pascal
+  nil
+//^^^ constant.language.null.pascal
+
 // comment
 procedure foo;
 // ^ meta.function.pascal keyword.declaration.function
