dfefdb8d0f00a93e1e518759cb8c707751dad586
deathaxe <deathaxe82@googlemail.com>
2025-02-24 21:58:13 +0100

    [Common] Add conflict markers in OCamllex/OCamlyacc block comments

--- OCaml/syntax_test_ml.ml
+++ OCaml/syntax_test_ml.ml
@@ -1,8 +1,6 @@
 (* SYNTAX TEST "Packages/OCaml/OCaml.sublime-syntax" *)
 
-(*
- * Merge Conflict Marker Tests
- *)
+(* Merge Conflict Marker Tests *)
 
 <<<<<<< HEAD
 (* <- meta.block.conflict.begin.diff punctuation.section.block.begin.diff *)
@@ -22,6 +20,27 @@
 (*     ^ meta.block.conflict.end.diff - entity - punctuation *)
 (*      ^^^^^^ meta.block.conflict.end.diff entity.name.section.diff *)
 (*            ^ meta.block.conflict.end.diff - entity - punctuation *)
+
+(** CONFLICT IN BLOCK COMMENT
+<<<<<<< HEAD
+(* <- meta.block.conflict.begin.diff punctuation.section.block.begin.diff *)
+(*^^^^^ meta.block.conflict.begin.diff punctuation.section.block.begin.diff *)
+(*     ^ meta.block.conflict.begin.diff - entity - punctuation *)
+(*      ^^^^ meta.block.conflict.begin.diff entity.name.section.diff *)
+(*          ^ meta.block.conflict.begin.diff - entity - punctuation *)
+
+=======
+(* <- meta.block.conflict.separator.diff punctuation.section.block.diff *)
+(*^^^^^ meta.block.conflict.separator.diff punctuation.section.block.diff *)
+(*     ^ meta.block.conflict.separator.diff - punctuation *)
+
+>>>>>>> master
+(* <- meta.block.conflict.end.diff punctuation.section.block.end.diff *)
+(*^^^^^ meta.block.conflict.end.diff punctuation.section.block.end.diff *)
+(*     ^ meta.block.conflict.end.diff - entity - punctuation *)
+(*      ^^^^^^ meta.block.conflict.end.diff entity.name.section.diff *)
+(*            ^ meta.block.conflict.end.diff - entity - punctuation *)
+** CONFLICT IN BLOCK COMMENT*)
 
   let open Core.Std
 (*^^^ keyword.other.ocaml *)
