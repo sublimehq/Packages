913c2de3ad219806e3eb70c8e107b28ae841d386
Johannes Rappen <jrappen@outlook.com>
2025-03-31 21:49:52 +0200

    [Groovy] refactor:
    - test base scope in syntax test file
    - bump syntax to v2, replace `pop: true` with `pop: 1`
    - move syntax variables to EOF
    - make capturing groups non-capturing where applicable
    - add section markers to syntax file
    - fix tests & add some more
    - fix comments & docs links
    - update functions
    - minor syntax file cleanup
    - fix numbers & do-while scope
    - rename & reorder string contexts
    - fix numbers
    - fix sequence scopes & contexts
    - fix operators & types
    - add syntax var for "optional arrays"
    - fix keywords
    - add some TODOs
    - fix shebang & add tests for it
    - fix metadata, add more named contexts
    - fix slashy strings with regexp
    - add build files
    - add keymap
    - add default settings
    - add `first_line_lang` syntax var
    - remove unused capturing group

---
+++ Groovy/tests/syntax_test_shebang.groovy
@@ -0,0 +1,4 @@
+#! SYNTAX TEST "Packages/Groovy/Groovy.sublime-syntax" groovy
+#! <- comment.line.shebang punctuation.definition.comment
+ #! <- comment.line.shebang punctuation.definition.comment
+#!                                                     ^^^^^^ comment.line.shebang constant.language.shebang
