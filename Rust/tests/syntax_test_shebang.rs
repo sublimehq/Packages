65644efcbc2065972b2fdad088832155acd816b2
deathaxe <16542113+deathaxe@users.noreply.github.com>
2025-04-16 21:29:58 +0200

    [Rust] Add support for shebang and frontmatter (#4217)
    Resolves #4164
    
    This commit ensures to keep first_line_match and syntax highlighting sync.
    
    Push away from main is required for `embed_scope` to be correctly applied if Rust is embedded.

---
+++ Rust/tests/syntax_test_shebang.rs
@@ -0,0 +1,4 @@
+#! SYNTAX TEST "Rust.sublime-syntax"
+#! <- comment.line.shebang.rust punctuation.definition.comment.rust
+ #! <- comment.line.shebang.rust punctuation.definition.comment.rust
+#!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ comment.line.shebang.rust - punctuation
