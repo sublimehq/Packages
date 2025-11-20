7a06dfe91440c5cd7abd60f6c0dd448c7fc5a8aa
deathaxe <16542113+deathaxe@users.noreply.github.com>
2025-11-20 20:57:46 +0000

    [JavaScript] Fix merge conflict markers in comments (#4378)
    This commit adds patterns to scope merge conflict markers in block comments.

--- JavaScript/tests/syntax_test_js.js
+++ JavaScript/tests/syntax_test_js.js
@@ -83,6 +83,30 @@
 // <- comment.line.other.js punctuation.definition.comment.js
 //^^^^^^^^^^^^^^^^^^^^ comment.line.other.js - meta.preprocessor
 
+/* Merge conflicts in comments
+
+<<<<<<< HEAD
+// <- meta.block.conflict.begin.diff punctuation.section.block.begin.diff
+//^^^^^ meta.block.conflict.begin.diff punctuation.section.block.begin.diff
+//     ^ meta.block.conflict.begin.diff - entity - punctuation
+//      ^^^^ meta.block.conflict.begin.diff entity.name.section.diff
+//          ^ meta.block.conflict.begin.diff - entity - punctuation
+
+=======
+// <- meta.block.conflict.separator.diff punctuation.section.block.diff
+//^^^^^ meta.block.conflict.separator.diff punctuation.section.block.diff
+//     ^ meta.block.conflict.separator.diff - punctuation
+
+>>>>>>> master
+// <- meta.block.conflict.end.diff punctuation.section.block.end.diff
+//^^^^^ meta.block.conflict.end.diff punctuation.section.block.end.diff
+//     ^ meta.block.conflict.end.diff - entity - punctuation
+//      ^^^^^^ meta.block.conflict.end.diff entity.name.section.diff
+//            ^ meta.block.conflict.end.diff - entity - punctuation
+*/
+
+/* Top-level conflicts in comments */
+
 <<<<<<< HEAD
 // <- meta.block.conflict.begin.diff punctuation.section.block.begin.diff
 //^^^^^ meta.block.conflict.begin.diff punctuation.section.block.begin.diff
