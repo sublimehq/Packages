5ebcd01699fc8ff8c778dbd7ac66357d7b3513da
deathaxe <16542113+deathaxe@users.noreply.github.com>
2025-11-21 08:23:37 +0000

    [Ruby] Fix merge conflict markers in comments (#4387)
    This commit adds patterns to scope merge conflict markers in block comments.

--- Ruby/syntax_test_ruby.rb
+++ Ruby/syntax_test_ruby.rb
@@ -31,6 +31,27 @@
 # Merge Conflicts
 ##################
 
+=begin
+<<<<<<< HEAD
+#  <- meta.block.conflict.begin.diff punctuation.section.block.begin.diff
+# ^^^^^ meta.block.conflict.begin.diff punctuation.section.block.begin.diff
+#      ^ meta.block.conflict.begin.diff - entity - punctuation
+#       ^^^^ meta.block.conflict.begin.diff entity.name.section.diff
+#           ^ meta.block.conflict.begin.diff - entity - punctuation
+
+=======
+#  <- meta.block.conflict.separator.diff punctuation.section.block.diff
+# ^^^^^ meta.block.conflict.separator.diff punctuation.section.block.diff
+#      ^ meta.block.conflict.separator.diff - punctuation
+
+>>>>>>> master
+#  <- meta.block.conflict.end.diff punctuation.section.block.end.diff
+# ^^^^^ meta.block.conflict.end.diff punctuation.section.block.end.diff
+#      ^ meta.block.conflict.end.diff - entity - punctuation
+#       ^^^^^^ meta.block.conflict.end.diff entity.name.section.diff
+#             ^ meta.block.conflict.end.diff - entity - punctuation
+=end
+
 <<<<<<< HEAD
 #  <- meta.block.conflict.begin.diff punctuation.section.block.begin.diff
 # ^^^^^ meta.block.conflict.begin.diff punctuation.section.block.begin.diff
