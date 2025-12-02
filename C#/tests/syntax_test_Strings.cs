1d4e26fc7cf3315eb6831c0a57090463eb825e90
Michael Lyons <github@michael.lyo.nz>
2025-12-01 23:15:07 -0500

    [C#] Include SQL in "long" strings

--- C#/tests/syntax_test_Strings.cs
+++ C#/tests/syntax_test_Strings.cs
@@ -1,5 +1,19 @@
 /// SYNTAX TEST "Packages/C#/C#.sublime-syntax"
 
+var character = 'y';
+/// ^^^^^^^^^ variable.other.cs
+///           ^ keyword.operator.assignment.variable.cs
+///             ^^^ meta.string.cs string.quoted.single.cs
+///             ^ punctuation.definition.string.begin.cs
+///              ^ constant.character.literal.cs
+///               ^ punctuation.definition.string.end.cs
+///                ^ punctuation.terminator.statement.cs
+var character_too_long = 'no';
+/// ^^^^^^^^^^^^^^^^^^ variable.other.cs
+///                    ^ keyword.operator.assignment.variable.cs
+///                      ^^^^ invalid.illegal.not_a_char.cs
+///                          ^ punctuation.terminator.statement.cs
+
 "short unicode \u1234";
 ///<- string.quoted.double.cs
 ///            ^^^^^^ constant.character.escape.cs
@@ -27,20 +41,20 @@
 ///                                           ^^ constant.character.escape.cs
 
 var literal = "foo";
-///           ^^^^^ string.quoted.double
+///           ^^^^^ string.quoted.double - meta.string.interpolated
 var interpolated_none = $"foo";
 ///                     ^^^^^^ meta.string.interpolated.cs string.quoted.double.cs
 var interpolated_yes = $"foo {bar} foo";
 ///                    ^^^^^^^^^^^^^^^^ meta.string.interpolated.cs
 var verbatim_singleline = @"foo";
-///                       ^^^^^^ string.quoted.double.raw.cs
+///                       ^^^^^^ meta.string.cs string.quoted.double.raw.cs - meta.string.interpolated
 var verbatim_singleline_interpolated_none = $@"foo bar";
 ///                                         ^^^^^^^^^^^ meta.string.interpolated.cs string.quoted.double.raw.cs
 var verbatim_singleline_interpolated_yes = $@"foo {bar} foo";
 ///                                        ^^^^^^^ string.quoted.double.raw.cs
 ///                                        ^^^^^^^^^^^^^^^^^ meta.string.interpolated.cs
 var verbatim_multiline = @"foo bar
-///                      ^^^^^^^^^^ string.quoted.double.raw.cs
+///                      ^^^^^^^^^^ meta.string.cs string.quoted.double.raw.cs - meta.string.interpolated
 baz";
 var verbatim_multiline_interpolated_none = $@"foo bar
 ///                                        ^^^^^^^^^^^ meta.string.interpolated.cs string.quoted.double.raw.cs
@@ -49,6 +63,44 @@ var verbatim_multiline_interpolated_yes = $@"foo {bar}
 ///                                       ^^^^^^ string.quoted.double.raw.cs
 ///                                       ^^^^^^^^^^^^ meta.string.interpolated.cs
 baz";
+
+var verbatim_singleline_sql = @"
+    SELECT  *
+    FROM    foo
+    WHERE   SQLi='{0}'";
+///^^^^^^^^^^^^^^^^^^^^ meta.string.cs string.quoted.double.raw.cs - meta.string.interpolated
+///^^^^^^^^^^^^^^^^^^^ source.sql
+/// ^^^^^ keyword.other.dml.sql
+///         ^^^^ meta.column-name.sql
+///             ^ keyword.operator.comparison.sql
+///              ^^^^^ meta.string.sql string.quoted.single.sql
+///              ^ punctuation.definition.string.begin.sql
+///               ^^^ constant.other.placeholder.cs
+///               ^ punctuation.definition.placeholder.begin.cs
+///                ^ meta.number.integer.decimal.cs constant.numeric.value.cs
+///                 ^ punctuation.definition.placeholder.end.cs
+///                  ^ punctuation.definition.string.end.sql
+///                   ^ punctuation.definition.string.end.cs
+///                    ^ punctuation.terminator.statement.cs
+
+var verbatim_singleline_sql_interpolated = $@"
+    SELECT  *
+    FROM    foo
+    WHERE   SQLi='{bar}'";
+///^^^^^^^^^^^^^^^^^^^^^^ meta.string.interpolated.cs string.quoted.double.raw.cs
+///^^^^^^^^^^^^^^^^^^^^^ source.sql
+/// ^^^^^ keyword.other.dml.sql
+///         ^^^^ meta.column-name.sql
+///             ^ keyword.operator.comparison.sql
+///              ^^^^^^^ meta.string.sql
+///              ^ string.quoted.single.sql punctuation.definition.string.begin.sql
+///               ^^^^^ meta.interpolation.cs
+///               ^ punctuation.section.interpolation.begin.cs
+///                ^^^ source.cs variable.other.cs
+///                   ^ punctuation.section.interpolation.end.cs
+///                    ^ string.quoted.single.sql punctuation.definition.string.end.sql
+///                     ^ punctuation.definition.string.end.cs
+///                      ^ punctuation.terminator.statement.cs
 
     "{32F31D43-81CC-4C15-9DE6-3FC5453562B6}"
 /// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ string.quoted.double
